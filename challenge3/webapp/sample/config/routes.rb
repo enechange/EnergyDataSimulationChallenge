4e6ab0a1cabc233aebc688f331cae4f6af627b8c
shirakia <atsuo.shiraki@gmail.com>
2014-03-22 22:26:44 +0000

    rails g scaffold energy label:integer house_id:integer year:integer month:integer temperature:decimal daylight:decimal energy_production:integer

--- challenge3/webapp/sample/config/routes.rb
+++ challenge3/webapp/sample/config/routes.rb
@@ -1,4 +1,6 @@
 Sample::Application.routes.draw do
+  resources :energies
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
