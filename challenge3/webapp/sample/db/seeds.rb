b3842906d1a69195ed0feacd54762048e0d4e796
shirakia <atsuo.shiraki@gmail.com>
2014-03-23 19:12:49 +0000

    Load seed data from csv

--- challenge3/webapp/sample/db/seeds.rb
+++ challenge3/webapp/sample/db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+# Read MIAC city codes from csv file
+CSV.foreach("../../data/dataset_50.csv", :headers => true) do |row|
+  Energy.create!(label:    row['Label'],
+                 house_id: row['House'],
+                 year:     row['Year'],
+                 month:    row['Month'],
+                 temperature:       row['Temperature'],
+                 daylight:          row['Daylight'],
+                 energy_production: row['EnergyProduction'],
+                 )
+end
+
+CSV.foreach("../../data/house_data.csv", :headers => true) do |row|
+  House.create!(firstname:     row['Firstname'],
+                lastname:      row['Lastname'],
+                city:          row['City'],
+                num_of_people: row['num_of_people'],
+                has_child:     row['has_child'],
+                )
+end
