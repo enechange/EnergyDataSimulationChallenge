627bb5f05d7a7f09bcbaee0d4925367c73826ded
Sungho Arai <sungho1031park@gmail.com>
2014-04-02 13:38:04 +0000

    Create a dataset for each column for providing each graph

--- challenge3/webapp/sungho_arai/app/assets/javascripts/angular/controllers/chartCtrl.js
+++ challenge3/webapp/sungho_arai/app/assets/javascripts/angular/controllers/chartCtrl.js
@@ -9,25 +9,51 @@ app.controller('graphCtrl', [
     $scope.init = function(id)
     {
       $scope.house_id = id;
+      $scope.date = [["Date"]];
+      $scope.production = [["Energy Production"]];
+      $scope.temperature = [["Temperature"]];
+      $scope.daylight = [["DayLight"]];
+
+      // TODO(sungho.arai): Rename variables for datapoints 
       var dataset = Data.query({id: $scope.house_id});
       dataset.$promise.then(function (dataset) {
-        var titles = [["Date", "Energy Production", "Temperature", "DayLight"]];
         dataset.forEach(function(data){
-          datapoint = new Array();
-          datapoint.push(data["Date"]);
-          datapoint.push(data["Production"]);
-          datapoint.push(data["Temperature"]);
-          datapoint.push(data["Daylight"]);
-          titles.push(datapoint);
+          datapoint0 = new Array();
+          datapoint0.push(data["Date"]);
+          $scope.date.push(datapoint0);
+
+          datapoint1 = new Array();
+          datapoint1.push(data["Production"]);
+          $scope.production.push(datapoint1);
+
+          datapoint2 = new Array();
+          datapoint2.push(data["Temperature"]);
+          $scope.temperature.push(datapoint2);
+
+          datapoint3 = new Array();
+          datapoint3.push(data["Daylight"]);
+          $scope.daylight.push(datapoint3);
          });
-        var data = google.visualization.arrayToDataTable(titles);
+
+        // TODO(sungho.arai): create a graph dynamically with radio button 
+        dataset = new Array();
+        for (var i =0; i< $scope.date.length; i++) {
+          datapoint = new Array();
+          datapoint.push($scope.date[i][0], $scope.production[i][0], $scope.daylight[i][0]);
+          dataset.push(datapoint);
+        }
+
         var chart = {};
-        var options = {};
+        var data = google.visualization.arrayToDataTable(dataset);
+        var options = {
+          seriesType: "bars"
+        };
 
         chart.data = data;
         chart.options = options;
         $scope.chart = chart;
-        var graph = new google.visualization.LineChart(document.getElementById('chartdiv'));
+
+        var graph = new google.visualization.ColumnChart(document.getElementById('chartdiv'));
         graph.draw($scope.chart.data, $scope.chart.options)
       });
     };
