a7431388908df5f94035a3d90ec5a66e88375ab1
Masayuki Tanaka <masayuki0812@mac.com>
2014-04-16 15:19:45 +0100

    Fix city energies to allow gap of data

--- challenge3/webapp/masayuki0812/app/models/city.rb
+++ challenge3/webapp/masayuki0812/app/models/city.rb
@@ -11,9 +11,13 @@ class City < ActiveRecord::Base
     cities.collect! { |city|
       if count_of_cities and count_of_cities.has_key?(city.id)
         city.count_house = count_of_cities[city.id.to_i]
+      elsif
+        city.count_house = 0
       end
       if total_energy_production_of_cities and total_energy_production_of_cities.has_key?(city.id)
         city.total_energy_production = total_energy_production_of_cities[city.id.to_i]
+      elsif
+        city.total_energy_production = 0
       end
       city
     }
@@ -24,44 +28,87 @@ class City < ActiveRecord::Base
     city = super(id)
     if with_count_house
       city.count_house = House.count_by_city_id(id)
+      if not city.count_house
+        city.count_house = 0
+      end
     end
     if with_total_energy_production
       city.total_energy_production = CityEnergy.sum_by_city_id(id)
+      if not city.total_energy_production
+        city.total_energy_production = 0
+      end
     end
     city
   end
 
   def self.energies(id=nil)
+    # TODO: This range should be given
+    # generate row keys explicitly to allow gap of data
+    target_dates = []
+    for y in [2011, 2012, 2013]
+      for m in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        next if (y == 2011 and m < 7) or (y == 2013 and 6 < m)
+        target_dates += ["%d-%02d" % [y, m]]
+      end
+    end
+
     if id
       city = find(id)
-      data = city ? city.city_energies.map{|e| ["#{e.year}-#{e.month}", e.energy_production]} : []
-      {
-        data: [["date", "Energy Production"]] + data,
+
+      # reconstruct guery result for easy result generation
+      data_by_date = {}
+      if city
+        city.city_energies.each { |d|
+          key = "%d-%02d" % [d.year, d.month]
+          data_by_date[key] = d.energy_production
+        }
+      end
+
+      # generate result rows
+      rows = [['date', "Energy Production"]]
+      target_dates.each { |target_date|
+        row = [target_date]
+        p target_date, data_by_date.has_key?(target_date)
+        if data_by_date.has_key?(target_date)
+          row += [data_by_date[target_date]]
+        elsif
+          row += [0]
+        end
+        rows += [row]
       }
     else
       cities = all().order("id")
       data = CityEnergy.all().order("city_id, year, month")
 
+      # reconstruct guery result for easy result generation
       data_by_date = {}
       data.each { |d|
         key = "%d-%02d" % [d.year, d.month]
         if not data_by_date.has_key?(key)
-          data_by_date[key] = []
+          data_by_date[key] = {}
         end
-        data_by_date[key] += [d.energy_production]
+        data_by_date[key][d.city_id] = d.energy_production
       }
 
-      header = ['date'] + cities.map { |city|
-        city.name
-      }
-      data = data_by_date.keys.sort.map { |key|
-        [key] + data_by_date[key]
-      }
-
-      {
-        data: [header] + data,
+      # generate result rows
+      rows = [['date'] + cities.map{ |city| city.name }]
+      target_dates.each { |target_date|
+        row = [target_date]
+        if data_by_date.has_key?(target_date)
+          d = data_by_date[target_date]
+          cities.each { |city|
+            row += [d.has_key?(city.id) ? d[city.id] : 0]
+          }
+        elsif
+          row += Array.new(cities.length, 0)
+        end
+        rows += [row]
       }
     end
+
+    {
+      data: rows
+    }
   end
 
 end
