dee72b189b8edc2f445812c1a027cb123e398abf
Tsutsumi Fukumu <levelfourslv@gmail.com>
2014-07-03 08:42:04 +0900

    setup rails project

---
+++ challenge3/webapp/levelfour/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Levelfour::Application.config.secret_key_base = '902e229d421195010484152277ce746d9edb0d70cd4a9549235c514cd7815bda8d8986296a71b2339beb9a5946c9fca3ef08580659a1ec2c12987ce50825f44c'
