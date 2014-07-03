bd290e192e8204e2ec231c59e71d7343ab98311c
Tsutsumi Fukumu <levelfourslv@gmail.com>
2014-07-03 09:16:20 +0900

    use MySQL as database

--- challenge3/webapp/levelfour/config/initializers/secret_token.rb
+++ challenge3/webapp/levelfour/config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Levelfour::Application.config.secret_key_base = '902e229d421195010484152277ce746d9edb0d70cd4a9549235c514cd7815bda8d8986296a71b2339beb9a5946c9fca3ef08580659a1ec2c12987ce50825f44c'
+Levelfour::Application.config.secret_key_base = '90f770643c3d3d2c1b78562b271ed8f17f31a6396ad6317fc979c24b043b66d2b1ee491a567b12e2fb0fc076858cb36dc9d0c6a712484aac02d161fe6bdec56f'
