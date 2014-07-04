746fe4d31b8731496a2e0ce13e34f8d5724db8fe
Tsutsumi Fukumu <levelfourslv@gmail.com>
2014-07-05 00:22:15 +0900

    add gem bootstrap-sass

--- challenge3/webapp/levelfour/config/application.rb
+++ challenge3/webapp/levelfour/config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(:default, Rails.env)
 
 module Levelfour
   class Application < Rails::Application
+	config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
