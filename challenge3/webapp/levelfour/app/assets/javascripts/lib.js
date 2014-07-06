ff6048958d17609b57f20d108bda9fdd7ae7e4d2
Tsutsumi Fukumu <levelfourslv@gmail.com>
2014-07-06 21:06:36 +0900

    use page:load event (respond to turbolink)

--- challenge3/webapp/levelfour/app/assets/javascripts/lib.js
+++ challenge3/webapp/levelfour/app/assets/javascripts/lib.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).on('ready page:load', function() {
   $('tr[data-href]').addClass('clickable').click(function(e) {
       if(!$(e.target).is('a')) {
         window.location = $(e.target).closest('tr').data('href');
