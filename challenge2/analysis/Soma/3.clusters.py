4600eafd7bf542fc9ea0d36976d53452b7d92ba7
soma11soma <h2so4cos0@gmail.com>
2015-07-11 21:55:42 +0800

    I fixed 3.clusters.py and added README.md

--- challenge2/analysis/Soma/3.clusters.py
+++ challenge2/analysis/Soma/3.clusters.py
@@ -1,47 +1,58 @@
-
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
+import pandas as pd
+from sklearn.mixture import GMM
+from sklearn.preprocessing import LabelEncoder
+import matplotlib as mpl
+from matplotlib import style
+style.use('ggplot')
+
+MY_FILE= 'total_watt.csv'
+
+#read MY_FILE---------------------------------------------------------------------------------------------
+
+df = pd.read_csv(MY_FILE, parse_dates=[0], index_col=[0])   #parse_dates decides which column to read in datetime and index_col---decide index column.
+df = df.resample('1D', how='sum')
+df = df.dropna()                                            #sklearn is not NaN tolerant.
+
+#set date and consumption---------------------------------------------------------------------------------
+
+date = df.index.tolist()                                    #tolist returns the array as a list
+date = [x.strftime('%m-%d') for x in date]
+le = LabelEncoder()                                         #LabelEncoder converts text data into numeric form.
+date = le.fit_transform(date)
+consumption = df[df.columns[0]].values
+
+#ser X, fig and ax-----------------------------------------------------------------------------------------
+
+X = np.array([date, consumption]).T                         #.T accesses the attribute T of the object.
+fig, ax = plt.subplots(figsize=(12,8))
+
+#to add spheres surrounding my plots-----------------------------------------------------------------------
+
+gmm = GMM(n_components=3)
+gmm.fit(X)                                                  #call 'fit'
+y_pred = gmm.predict(X)
+for i, color in enumerate('grb'):
+    width, height = 2 * 1.96 * np.sqrt(np.diagonal(gmm._get_covars()[i]))
+    ell = mpl.patches.Ellipse(gmm.means_[i], width, height, color=color)
+    ell.set_alpha(0.3)
+    ax.add_artist(ell)
+    Xdata = X[y_pred == i]
+    ax.scatter(Xdata[:,0], Xdata[:,1], color=color)
+    ax.scatter(gmm.means_[i][0], gmm.means_[i][1], marker='x', s=300, c=color)
 
 
+#setting title, x/y label and ticks. ----------------------------------------------------------------------------------
 
-MY_FILE = 'total_watt.csv'
+a = np.arange(0, len(X), 3)
+ax.set_xticks(a)
+ax.set_xticklabels(le.inverse_transform(a.astype(int)))     #to set the x-ticks as dates
+ax.set_title('Energy consumptions Clusters (high/medium/low)')
+ax.set_xlabel('date (year 2011)')
+ax.set_ylabel('energy consumption')
+ax.tick_params(axis='x', colors='midnightblue')
+ax.tick_params(axis='y', colors='midnightblue')
 
-df = pd.read_csv(MY_FILE, parse_dates=[0], header=None, names=['datetime', 'consumption'])
-
-df['date'] = [x.date() for x in df['datetime']]
-df['time'] = [x.time() for x in df['datetime']]
-
-pv = df.pivot(index='time', columns='date', values='consumption')
-
-# to avoid holes in the surface
-pv = pv.fillna(0.0)
-
-xx, yy = np.mgrid[0:len(pv),0:len(pv.columns)]
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-surf=ax.plot_surface(xx, yy, pv.values, cmap='jet', cstride=1, rstride=1)
-
-fig.colorbar(surf, shrink=0.5, aspect=10)
-
-dates = [x.strftime('%m-%d') for x in pv.columns]
-times = [x.strftime('%H:%M') for x in pv.index]
-
-ax.set_title('Energy consumptions Clusters', color='lightseagreen')
-ax.set_xlabel('time', color='darkturquoise')
-ax.set_ylabel('date(year 2011)', color='darkturquoise')
-ax.set_zlabel('energy consumption', color='darkturquoise')
-ax.set_xticks(xx[::10,0])
-ax.set_xticklabels(times[::10], color='lightseagreen')
-ax.set_yticks(yy[0,::10])
-ax.set_yticklabels(dates[::10], color='lightseagreen')
-
-ax.set_axis_bgcolor('black')
-
+#finally!
 plt.show()
-
-
-#Thanks for reading! Looking forward to the Skype Interview.
---
+++ challenge2/analysis/Soma/README.md
@@ -0,0 +1,26 @@
+
+
+Considering that Enechange is a service which offers an electricity plan to each household, I assume that the total_watt.csv data is about electricity consumption in Japanese households and proceed to a discussion.
+
+The total_watt.csv leaves out energy consumption data between 5/3 21:52 - 5/6 13:52. This is because this period includes holidays in Japan. By excluding the data in this period, we can also exclude unexpected outliers.
+
+・from 1.30mins.jpg--------------------------------------------------------
+
+There are some particular points where electricity consumption is extremely high which may indicate the time when many people use electrical devices in their house, such as air-conditioner, lights and the TV (these three electric devices occupy 60% of electric energy consumption in Japan.). You can also check this from extra3D.jpg
+
+・from 2.day.jpg--------------------------------------------------------
+
+The indicated points on the graph are the weekends. As you can see from the graph, electricity consumption is high around weekends, which also suggests total_watt.csv treats energy consumption in households, not in companies as corporation offices are usually closed on weekends.  You can also find a less varied jagged pattern in the graph, which shows normal workdays of the week, although I cannot find particular pattern for each day of the week. You can also check this from extra3D.jpg
+
+・from 3.clusters.jpg--------------------------------------------------------
+
+From this graph, you can find that, comparing to April, energy consumption is relatively low in May. This is because there is the special holiday at the beginning of May as I mentioned before. In this period, many households go out and do not consume much electricic energy in their house. Ideally, every households can take a break within this special holiday period, but some head of households cannot get a vacation in within this period and they take a vacation after special vacation period. This may explain why, following the special vacation period, energy consumption is still low. In addition, compared to April, it is much warmer in May. As a consequence, many households stop using air-conditioner in May, although it can become too hot at the end of May and some households start to use air-conditioner again.
+
+・from extra3D.jpg--------------------------------------------------------
+
+This is just a extra for this assignment.
+As you can see rom this graph, people don’t consume much energy from 04:22 to 10:22, which also suggests total_watt.csv treats energy consumption in households, not in company.
+
+
+
+Thanks for reading! Looking forward to the Skype Interview.
