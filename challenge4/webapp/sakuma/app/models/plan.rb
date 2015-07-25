13329675ced45806c2d828ea276d2f80e4594d1a
Naohiro Sakuma <nao@NaoiMac.local>
2015-07-25 21:04:27 +0900

    Implement rate plan from plans.json

---
+++ challenge4/webapp/sakuma/app/models/plan.rb
@@ -0,0 +1,6 @@
+class Plan
+  include ActiveModel::Model
+
+  attr_accessor :name, :day_time, :night_time, :night_time_range
+
+end
--- challenge4/webapp/sakuma/config/application.rb
+++ challenge4/webapp/sakuma/config/application.rb
@@ -17,6 +17,9 @@ Bundler.require(*Rails.groups)
 
 module EnergyChargeCalculator
   class Application < Rails::Application
+
+    config.autoload_paths << Rails.root.join('lib')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
