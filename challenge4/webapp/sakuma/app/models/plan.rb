6246e2994b3d13396645c313de46e1ced8d68fea
Naohiro Sakuma <nao@NaoiMac.local>
2015-07-25 21:51:11 +0900

    Implement day time units

--- challenge4/webapp/sakuma/app/models/plan.rb
+++ challenge4/webapp/sakuma/app/models/plan.rb
@@ -14,4 +14,12 @@ class Plan
     night_time_range[array_index].present?
   end
 
+  def day_time_unit
+    {
+      first:  {from: day_time[0][0], to: day_time[0][1], unit: day_time[0][2], cons: 0.0},
+      second: {from: day_time[1][0], to: day_time[1][1], unit: day_time[1][2], cons: 0.0},
+      third:  {from: day_time[2][0], to: day_time[2][1], unit: day_time[2][2], cons: 0.0},
+    }
+  end
+
 end
--- challenge4/webapp/sakuma/spec/models/plan_spec.rb
+++ challenge4/webapp/sakuma/spec/models/plan_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Plan, type: :model do
 
-  let(:no_night_plan) do
+  let(:only_day_time_plan) do
     Plan.new(
       name: "Day time only",
       day_time: [ [ nil, 120, 19.43], [ 120, 300, 25.91], [ 300, nil, 29.93] ],
@@ -29,7 +29,7 @@ RSpec.describe Plan, type: :model do
     subject { this_plan.has_night_plan? }
 
     context 'when no night plan' do
-      let(:this_plan) { no_night_plan }
+      let(:this_plan) { only_day_time_plan }
       it { is_expected.to be false }
     end
 
@@ -67,7 +67,7 @@ RSpec.describe Plan, type: :model do
     end
 
     context 'about no night time plan' do
-      let(:this_plan) { no_night_plan }
+      let(:this_plan) { only_day_time_plan }
 
       it_should_behave_like 'should not be night', 1
       it_should_behave_like 'should not be night', 5
@@ -76,6 +76,33 @@ RSpec.describe Plan, type: :model do
       it_should_behave_like 'should not be night', 21
       it_should_behave_like 'should not be night', 22
       it_should_behave_like 'should not be night', 24
+    end
+  end
+
+  describe '#day_time_units' do
+
+    subject { plan.day_time_unit }
+
+    context 'abount only day time plan' do
+      let(:plan) { only_day_time_plan }
+      it do
+        is_expected.to eq({
+          first:  {from: nil, to: 120, unit: 19.43, cons: 0.0},
+          second: {from: 120, to: 300, unit: 25.91, cons: 0.0},
+          third:  {from: 300, to: nil, unit: 29.93, cons: 0.0},
+        })
+      end
+    end
+
+    context 'abount night time plan' do
+      let(:plan) { night_plan}
+      it do
+        is_expected.to eq({
+          first:  {from: nil, to: 90, unit: 24.03, cons: 0.0},
+          second: {from: 90, to: 230, unit: 32.03, cons: 0.0},
+          third:  {from: 230, to: nil, unit: 37.00, cons: 0.0},
+        })
+      end
     end
   end
 
