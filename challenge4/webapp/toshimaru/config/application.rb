13bcfe328e2127169b8a911e7958af329618c3a2
toshi <me@toshimaru.net>
2016-02-25 00:35:06 +0900

    autoload lib

--- challenge4/webapp/toshimaru/config/application.rb
+++ challenge4/webapp/toshimaru/config/application.rb
@@ -26,5 +26,7 @@ module Toshimaru
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths << "#{Rails.root}/lib"
   end
 end
