413ab342bbe1c0f50b862be7442d04994019cdcf
toshi <me@toshimaru.net>
2016-03-09 00:35:01 +0900

    Add initial specs

--- challenge4/webapp/toshimaru/spec/controllers/plans_controller_spec.rb
+++ challenge4/webapp/toshimaru/spec/controllers/plans_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PlansController, :type => :controller do
+  let(:input) { [([0.3] * 24)] * 30 }
 
+  describe "#calculate" do
+    it "returns 200" do
+      post :calculate
+      expect(response.status).to eq(200)
+    end
+
+    it "returns 200" do
+      post :calculate
+      expect(response.header['Content-Type']).to include 'application/json'
+    end
+  end
 end
