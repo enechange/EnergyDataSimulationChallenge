305682673bcf24c39b70e9765faa30dd8e574e54
toshi <me@toshimaru.net>
2016-03-09 01:49:20 +0900

    post raw JSON

--- challenge4/webapp/toshimaru/spec/controllers/plans_controller_spec.rb
+++ challenge4/webapp/toshimaru/spec/controllers/plans_controller_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe PlansController, :type => :controller do
-  let(:input) { [([0.3] * 24)] * 30 }
-
   describe "#calculate" do
+    before do
+      request.env["CONTENT_TYPE"] = 'application/json'
+      request.env["HTTP_ACCEPT"]  = 'application/json'
+    end
+
+    let(:input) { [([0.3] * 24)] * 30 }
+    let(:input_json) { {usage: input}.to_json }
+
     it "returns 200" do
-      post :calculate
+      post :calculate, input_json
       expect(response.status).to eq(200)
     end
 
     it "returns 200" do
-      post :calculate
+      post :calculate, input_json
       expect(response.header['Content-Type']).to include 'application/json'
     end
   end
