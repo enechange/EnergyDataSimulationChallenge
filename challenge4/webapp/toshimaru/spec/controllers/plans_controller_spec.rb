a97f11d269d9332f3a8b4619b9872bc80a18d137
toshi <me@toshimaru.net>
2016-03-14 23:21:44 +0900

    Add a day usage test

--- challenge4/webapp/toshimaru/spec/controllers/plans_controller_spec.rb
+++ challenge4/webapp/toshimaru/spec/controllers/plans_controller_spec.rb
@@ -38,11 +38,39 @@ RSpec.describe PlansController, :type => :controller do
 
     describe "with fixture" do
       let(:input_path) { "#{fixture_path}/input" }
-      let(:sample_input) { File.read("#{input_path}/sample-input.json") }
 
-      it "WIP" do
-        post :calculate, sample_input
-        puts "output: #{response.body}"
+      describe "month usage (sample-input.json)" do
+        let(:sample_input) { File.read("#{input_path}/sample-input.json") }
+
+        before do
+          post :calculate, sample_input
+          @result = JSON.parse(response.body)
+        end
+
+        it "Meter-Rate Lighting B" do
+          expect(@result.first["price"]).to eq 2532
+        end
+
+        it "Yoru Toku Plan" do
+          expect(@result.second["price"]).to eq 2697
+        end
+      end
+
+      describe "a day usage" do
+        let(:sample_input2) { File.read("#{input_path}/sample-input-1day-usage.json") }
+
+        before do
+          post :calculate, sample_input2
+          @result = JSON.parse(response.body)
+        end
+
+        it "Meter-Rate Lighting B" do
+          expect(@result.first["price"]).to  eq 80
+        end
+
+        it "WIP" do
+          expect(@result.second["price"]).to eq 86
+        end
       end
     end
   end
