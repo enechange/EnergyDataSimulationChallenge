91e34844881c32ecba424e7e76b02e02481e69f9
Mari Murotani <muromari5123@gmail.com>
2018-10-14 10:36:03 +0900

    [develop] Added a column to data schema & done to implement csv_loader:data_set

--- challenge3/webapp/m_murotani/chart_example/db/migrate/20181013131553_create_data_set_samples.rb
+++ challenge3/webapp/m_murotani/chart_example/db/migrate/20181013131553_create_data_set_samples.rb
@@ -7,7 +7,7 @@ class CreateDataSetSamples < ActiveRecord::Migration[5.2]
       t.integer :month
       t.float :temperature
       t.float :day_light
-
+      t.integer :engery_production
       t.timestamps
     end
     add_index :data_set_samples, :year
