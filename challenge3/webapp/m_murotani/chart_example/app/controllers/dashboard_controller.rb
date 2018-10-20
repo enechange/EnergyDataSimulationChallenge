fc524aa0af209134c4063b20027586569b4db890
Mari Murotani <muromari5123@gmail.com>
2018-10-20 13:50:34 +0900

    [develop] code refactoring

--- challenge3/webapp/m_murotani/chart_example/app/controllers/dashboard_controller.rb
+++ challenge3/webapp/m_murotani/chart_example/app/controllers/dashboard_controller.rb
@@ -2,8 +2,8 @@ class DashboardController < ApplicationController
   def index
   end
   def show
-    city_sum = HouseData.group(:city).sum(:num_of_people)
-    temperature_avg = DataSetSample.group(:year).average(:temperature)
+    city_sum = HouseData.get_average_tempreture
+    temperature_avg = DataSetSample.get_average_data
     render json: {city_sum: city_sum, temperature_avg: temperature_avg}
   end
 end
