5c99cf8cb5a6690edbd8ee653ed9eb318e9ad7f1
y-shinada <ys09gm@gmail.com>
2019-01-20 14:49:18 +0900

    nginx, puma 設定追加

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,9 +7,10 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+set :user, 'y-shinada'
 set :rails_env, 'staging'
 
-set :branch, 'master'
+set :branch, ENV['BRANCH'] || 'master'
 
 set :bundle_flags, '--deployment'
 set :bundle_path, shared_path.join('vendor', 'bundle')
@@ -19,7 +20,32 @@ set :rbenv_ruby, '2.5.3'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
-server "54.178.170.155", user: "y-shinada", roles: %w{app db}, ssh_options: {
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log, "#{release_path}/log/puma.error.log"
+set :puma_preload_app, true
+set :puma_tag, fetch(:application)
+set :puma_worker_timeout, 60
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+set :nginx_flag_options, {
+  max_fails: 2,
+  fail_timeout: 60,
+}
+set :nginx_flags, fetch(:nginx_flag_options).to_a.map { |a| a.join('=') }.join(' ')
+set :nginx_server_names, [
+  "localhost",
+  "staging.hello-energy-personal.net",
+]
+set :nginx_server_name, fetch(:nginx_server_names).join(' ')
+set :nginx_socket_flags, fetch(:nginx_flags)
+# set :nginx_ssl_certificate, "/etc/ssl/certs/#{fetch(:nginx_config_name)}.crt"
+# set :nginx_ssl_certificate_key, "/etc/ssl/private/#{fetch(:nginx_config_name)}.key"
+set :nginx_use_ssl, false
+
+server "54.178.170.155", user: fetch(:user), roles: %w{app db web}, ssh_options: {
   port: 22,
   forward_agent: true,
   keys: ['config/certs/hello-energy-pem.pem'],
