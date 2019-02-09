e0301fe8946d1dc1313f308177de79904f00f49b
user <ryoc13@gmail.com>
2019-02-09 22:19:03 +0900

    Update houses_controller.rb

--- challenge3/webapp/ryia/app/controllers/houses_controller.rb
+++ challenge3/webapp/ryia/app/controllers/houses_controller.rb
@@ -1,2 +1,9 @@
 class HousesController < ApplicationController
+  def show
+    @avg_daylight          = Energy.group(:month).average(:daylight)
+    @avg_energy_production = Energy.group(:month).average(:energy_production)
+    @avg_temperature       = Energy.group(:month).average(:temperature)
+    @data                  = House.joins(:energies).select("houses.*, energies.*")
+    @sum_energy_production = @data.group(:city).sum(:energy_production)
+  end
 end
