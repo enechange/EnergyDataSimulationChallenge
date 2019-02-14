8acba1996f130414357735e7c35add179cd5c1ea
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2019-02-14 10:39:15 +0900

    Implemented bulk insert and modified service object

--- challenge3/webapp/hnishioka/app/models/energy_production.rb
+++ challenge3/webapp/hnishioka/app/models/energy_production.rb
@@ -1,19 +1,6 @@
 class EnergyProduction < ApplicationRecord
   belongs_to :house_energy_usage, foreign_key: 'house'
 
-  def self.import(path)
-    require 'csv'
-
-    CSV.foreach(
-      path,
-      headers: true,
-      header_converters: ->(h) { h.underscore.downcase }
-    ) do |row|
-
-      create!(row.to_h)
-    end
-  end
-
   def date_label
     "#{year}/#{month}"
   end
