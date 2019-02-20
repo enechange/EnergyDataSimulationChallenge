6403122f3825bf5140cbf56591b423c280a8e427
minojiro <minojiro@users.noreply.github.com>
2019-02-20 23:13:22 +0900

    use activerecord-import to bulk insert

--- challenge3/webapp/minojiro/db/seeds.rb
+++ challenge3/webapp/minojiro/db/seeds.rb
@@ -1,19 +1,19 @@
 require 'csv'
 
 house_csv = CSV.read("#{Rails.root.to_s}/tmp/house_data.csv", headers: true)
-house_csv.map{|r|
-  House.create(
+House.import(house_csv.map{|r|
+  House.new(
     csv_house_id:  r['ID'].to_i,
     firstname:     r['Firstname'],
     lastname:      r['Lastname'],
     city:          r['City'],
     num_of_people: r['num_of_people'].to_i,
     has_child:     r['has_child'] == 'Yes')
-}
+})
 
 energy_csv = CSV.read("#{Rails.root.to_s}/tmp/dataset_50.csv", headers: true)
-energy_csv.map{|r|
-  Energy.create(
+Energy.import(energy_csv.map{|r|
+  Energy.new(
     csv_energy_id:     r['ID'],
     label:             r['Label'],
     year:              r['Year'],
@@ -22,4 +22,4 @@ energy_csv.map{|r|
     daylight:          r['Daylight'],
     energy_production: r['EnergyProduction'],
     house:             House.find_by(csv_house_id: r['House']))
-}
+})
