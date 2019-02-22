7ee9b87752f32723c69425f5aa9f6a1fb02dc037
minojiro <minojiro@users.noreply.github.com>
2019-02-22 22:42:16 +0900

    use transaction when db:seed

--- challenge3/webapp/minojiro/db/seeds.rb
+++ challenge3/webapp/minojiro/db/seeds.rb
@@ -1,25 +1,28 @@
 require 'csv'
 
 house_csv = CSV.read("#{Rails.root.to_s}/data/house_data.csv", headers: true)
-House.import(house_csv.map{|r|
-  House.new(
-    csv_house_id:  r['ID'].to_i,
-    firstname:     r['Firstname'],
-    lastname:      r['Lastname'],
-    city:          r['City'],
-    num_of_people: r['num_of_people'].to_i,
-    has_child:     r['has_child'] == 'Yes')
-})
-
 energy_csv = CSV.read("#{Rails.root.to_s}/data/dataset_50.csv", headers: true)
-Energy.import(energy_csv.map{|r|
-  Energy.new(
-    csv_energy_id:     r['ID'],
-    label:             r['Label'],
-    year:              r['Year'],
-    month:             r['Month'],
-    temperature:       r['Temperature'],
-    daylight:          r['Daylight'],
-    energy_production: r['EnergyProduction'],
-    house:             House.find_by(csv_house_id: r['House']))
-})
+
+ActiveRecord::Base.transaction do
+  House.import(house_csv.map{|r|
+    House.new(
+      csv_house_id:  r['ID'].to_i,
+      firstname:     r['Firstname'],
+      lastname:      r['Lastname'],
+      city:          r['City'],
+      num_of_people: r['num_of_people'].to_i,
+      has_child:     r['has_child'] == 'Yes')
+  })
+
+  Energy.import(energy_csv.map{|r|
+    Energy.new(
+      csv_energy_id:     r['ID'],
+      label:             r['Label'],
+      year:              r['Year'],
+      month:             r['Month'],
+      temperature:       r['Temperature'],
+      daylight:          r['Daylight'],
+      energy_production: r['EnergyProduction'],
+      house:             House.find_by(csv_house_id: r['House']))
+  })
+end
