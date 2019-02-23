655f36a4bfad6bf2e04552c72d2403ec22b4470a
terapasta <hiro.nnsa724@gmail.com>
2019-02-23 10:10:17 +0900

    import_csvロジック追加

--- challenge3/webapp/hiroaki_terayama/app/models/house.rb
+++ challenge3/webapp/hiroaki_terayama/app/models/house.rb
@@ -1,12 +1,32 @@
+require 'csv'
 class House < ApplicationRecord
   enum has_child: { Yes: true, No: false }
 
   has_many :energy_records
 
-  validates :origin_id, presence: true, numericality: { only_integer: true }
+  validates :origin_id, presence: true, uniqueness: true, numericality: { only_integer: true }
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :city, presence: true
   validates :num_of_people, presence: true, numericality: { only_integer: true }
-  validates :has_child, presence: true,
+  validates :has_child, presence: true
+
+  def self.import_csv(path)
+    begin
+      CSV.foreach(path, headers: true) do |row|
+        data = House.find_by(origin_id: row['ID'].to_i) || new
+        data.attributes = {
+            origin_id: row['ID'].to_i,
+            firstname: row['Firstname'],
+            lastname: row['Lastname'],
+            city: row['City'],
+            num_of_people: row['num_of_people'].to_i,
+            has_child: row['has_child']
+        }
+        data.save!
+      end
+    end
+  rescue
+    # TODO: 例外処理する
+  end
 end
