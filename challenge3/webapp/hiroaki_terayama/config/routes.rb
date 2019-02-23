3f13e5d816b33363791890dfd21ed5fe41806738
terapasta <hiro.nnsa724@gmail.com>
2019-02-23 11:37:03 +0900

    csvのアップロード機能を追加

--- challenge3/webapp/hiroaki_terayama/config/routes.rb
+++ challenge3/webapp/hiroaki_terayama/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :houses, only: [:new, :create]
+  resources :energy_records, only: [:new, :create]
 end
