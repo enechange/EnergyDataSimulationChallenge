d7f48220633d50b073e2c6775e27cd72e906f0b3
terapasta <hiro.nnsa724@gmail.com>
2019-02-23 10:09:50 +0900

    migrationファイル修正

--- challenge3/webapp/hiroaki_terayama/db/migrate/20190222151908_create_houses.rb
+++ challenge3/webapp/hiroaki_terayama/db/migrate/20190222151908_create_houses.rb
@@ -1,12 +1,12 @@
 class CreateHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :houses do |t|
-      t.integer :houses, :origin_id, null: false
-      t.string :houses, :firstname, null: false
-      t.string :houses, :lastname, null: false
-      t.string :houses, :city, null: false
-      t.integer :houses, :num_of_people, null: false
-      t.boolean :houses, :has_child, default: false, null: false
+      t.integer :origin_id, null: false
+      t.string :firstname, null: false
+      t.string :lastname, null: false
+      t.string :city, null: false
+      t.integer :num_of_people, null: false
+      t.boolean :has_child, default: false, null: false
 
       t.timestamps
     end
--- challenge3/webapp/hiroaki_terayama/db/migrate/20190222155040_create_energy_records.rb
+++ challenge3/webapp/hiroaki_terayama/db/migrate/20190222155040_create_energy_records.rb
@@ -2,14 +2,14 @@ class CreateEnergyRecords < ActiveRecord::Migration[5.2]
   def change
     create_table :energy_records do |t|
       t.references :house, foreign_key: true
-      t.integer :energy_records, :origin_id, null: false
-      t.integer :energy_records, :label, null: false
-      t.integer :energy_records, :house, null: false
-      t.integer :energy_records, :year, null: false
-      t.integer :energy_records, :month, null: false
-      t.float :energy_records, :temperature, null: false
-      t.float :energy_records, :daylight, null: false
-      t.integer :energy_records, :energy_production, null: false
+      t.integer :origin_id, null: false
+      t.integer :label, null: false
+      t.integer :house_origin_id, null: false
+      t.integer :year, null: false
+      t.integer :month, null: false
+      t.float :temperature, null: false
+      t.float :daylight, null: false
+      t.integer :energy_production, null: false
 
       t.timestamps
     end
