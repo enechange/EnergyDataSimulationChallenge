d235432b506ddb4146d9dd3c5710998d4be13983
terapasta <hiro.nnsa724@gmail.com>
2019-02-26 23:28:10 +0900

    文言修正

--- challenge3/webapp/hiroaki_terayama/app/javascript/components/VJudge.vue
+++ challenge3/webapp/hiroaki_terayama/app/javascript/components/VJudge.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <div class="card-header">
-      <div class="card-header-title">Judge</div>
+      <div class="card-header-title">Judge result</div>
     </div>
     <div class="card-content">
       <div v-if="isJudged">You will get <span class="is-size-1" v-bind:class="{ 'has-text-danger': isMore, 'has-text-primary': !isMore }">{{ judge() }}</span> energy than average !</div>
