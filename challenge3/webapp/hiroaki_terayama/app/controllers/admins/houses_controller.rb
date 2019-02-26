e63553f6f9e43edf36873705fb5f83c01ee21939
terapasta <hiro.nnsa724@gmail.com>
2019-02-26 23:42:42 +0900

    管理者用ホーム画面を追加

---
+++ challenge3/webapp/hiroaki_terayama/app/controllers/admins/houses_controller.rb
@@ -0,0 +1,16 @@
+module Admins
+  class HousesController < ApplicationController
+
+    def new
+    end
+
+    def create
+      errors = House.import_csv(params[:csv_file].path)
+      if !errors
+        redirect_to new_house_path, flash: { success: 'インポート成功！' }
+      else
+        render :new, @message
+      end
+    end
+  end
+end
