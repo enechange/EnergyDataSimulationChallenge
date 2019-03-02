d358114a8e3839d57178f3ad398e52bf7ad422d5
Ryu007 <ryuta.tashiro@gmail.com>
2019-03-02 22:21:57 +0900

    modify energies_controller.rb

--- challenge3/webapp/ryuta_t/app/controllers/energies_controller.rb
+++ challenge3/webapp/ryuta_t/app/controllers/energies_controller.rb
@@ -1,13 +1,46 @@
 class EnergiesController < ApplicationController
-  def new
-  end
-
-  def edit
-  end
 
   def index
+    @energies = Energy.all
   end
 
   def show
+    @energy = Energy.find(params[:id])
   end
+
+  def new
+    @energy = Energy.new
+  end
+
+  def create
+    @energy = Energy.new(energy_params)
+    if @energy.save
+      flash[:success] = "データが追加されました"
+      redirect_to root_url
+    else
+      flash[:danger] = "データの追加に失敗しました"
+      render new_energy_path
+    end
+  end
+
+  def edit
+    @energy = Energy.find(params[:id])
+  end
+
+  def update
+    @energy = Energy.find(params[:id])
+    if @energy.update_attributes(energy_params)
+      flash[:success] = "データの更新に成功しました"
+      redirect_to root_url
+    else
+      flash[:danger] = "データの更新に失敗しました"
+      render edit_energy_ath
+    end
+  end
+
+  private
+
+    def energy_params
+      params.require(:energy).permit(:label, :house_id, :year, :month, :temperature, :daylight, :energy_production)
+    end
 end
