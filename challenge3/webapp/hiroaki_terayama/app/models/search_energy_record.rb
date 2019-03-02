7506d82e84460d054eb9a722e6ce6a6aa9e6aaf7
terapasta <hiro.nnsa724@gmail.com>
2019-03-02 20:31:18 +0900

    include modelを削除

--- challenge3/webapp/hiroaki_terayama/app/models/search_energy_record.rb
+++ challenge3/webapp/hiroaki_terayama/app/models/search_energy_record.rb
@@ -1,11 +1,8 @@
 class SearchEnergyRecord
-  include ActiveModel::Model
 
-  attr_accessor :city
-
-  def search
+  def self.search(args)
     rel = EnergyRecord
-    rel = rel.search_by_city(self.city) if self.city.present?
+    rel = rel.search_by_city(args['city']) if args && args['city'].present?
     rel
   end
 end
