8232c663270b1dacdecf6d7be070f7a55cf28281
Ryu007 <ryuta.tashiro@gmail.com>
2019-03-02 21:57:47 +0900

    add houses_controller

---
+++ challenge3/webapp/ryuta_t/app/controllers/houses_controller.rb
@@ -0,0 +1,13 @@
+class HousesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- challenge3/webapp/ryuta_t/config/routes.rb
+++ challenge3/webapp/ryuta_t/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'energies#index'
 
   resources :energies
+  resources :houses
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
