7a1a63a57f40358dbde0f0b605c14d815260f0d5
Ryu007 <ryuta.tashiro@gmail.com>
2019-03-02 21:53:22 +0900

    add energies_controller

---
+++ challenge3/webapp/ryuta_t/app/controllers/energies_controller.rb
@@ -0,0 +1,13 @@
+class EnergiesController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- challenge3/webapp/ryuta_t/config/routes.rb
+++ challenge3/webapp/ryuta_t/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'energies#index'
+
+  resources :energies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
