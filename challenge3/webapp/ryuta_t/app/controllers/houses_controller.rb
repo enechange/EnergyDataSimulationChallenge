4d71c6e51a2abaa3dd3bdf3ba731ab01c9b4121a
Ryu007 <ryuta.tashiro@gmail.com>
2019-03-02 22:33:11 +0900

    modify houses_controller.rb

--- challenge3/webapp/ryuta_t/app/controllers/houses_controller.rb
+++ challenge3/webapp/ryuta_t/app/controllers/houses_controller.rb
@@ -1,13 +1,48 @@
 class HousesController < ApplicationController
   def index
+    @houses = House.all
   end
 
   def show
+    @house = House.find(params[:id])
   end
 
   def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(house_params)
+    if @house.save
+      flash[:success] = "データの追加に成功しました"
+      redirect_to root_url
+    else
+      flash[:danger] = "データの追加に失敗しました"
+      render new_house_path
+    end
   end
 
   def edit
+    @house = House.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:id])
+    if @house.update_attributes(house_params)
+      flash[:success] = "データの更新に成功しました"
+      redirect_to root_url
+    else
+      flash[:danger] = "データの更新に失敗しました"
+      render edit_house_path
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def house_params
+    params.require(:house).permit(:firstname, :lastname, :city, :num_of_people, :has_child)
   end
 end
