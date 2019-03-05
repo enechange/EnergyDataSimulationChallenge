77a4212058b14c23c5147029e9ef6c5dbc6a80e9
terapasta <hiro.nnsa724@gmail.com>
2019-03-06 08:39:56 +0900

    リファクタリング

--- challenge3/webapp/hiroaki_terayama/app/javascript/components/VJudge.vue
+++ challenge3/webapp/hiroaki_terayama/app/javascript/components/VJudge.vue
@@ -4,7 +4,7 @@
       <div class="card-header-title">Judge result</div>
     </div>
     <div class="card-content">
-      <div v-if="aveSelected">You will get <span class="is-size-1" v-bind:class="{ 'has-text-danger': isMore, 'has-text-primary': !isMore }">{{ getJudge }}</span> energy than average !</div>
+      <div v-if="aveSelected">You will get <span class="is-size-1" v-bind:class="{ 'has-text-danger': isMore, 'has-text-primary': !isMore }">{{ judge }}</span> energy than average !</div>
       <div v-else>Not judged yet !</div>
     </div>
   </div>
@@ -20,20 +20,12 @@
     },
     watch: {
       aveSelected () {
-        if (this.aveAll < this.aveSelected) {
-          this.isMore = true
-        } else {
-          this.isMore = false
-        }
+        this.isMore = this.aveAll < this.aveSelected
       }
     },
     computed: {
-      getJudge () {
-        if (this.isMore) {
-          return 'more'
-        } else {
-          return 'less'
-        }
+      judge () {
+        return this.isMore ? 'more' : 'less'
       }
     }
   }
