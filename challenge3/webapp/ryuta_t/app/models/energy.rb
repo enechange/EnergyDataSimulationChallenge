a14537a65e6e8c07238d4cebb4cb3619083c257f
Ryu007 <ryuta.tashiro@gmail.com>
2019-03-10 19:49:06 +0900

    Clean energy.rb and energy_idndex_chart.html

--- challenge3/webapp/ryuta_t/app/models/energy.rb
+++ challenge3/webapp/ryuta_t/app/models/energy.rb
@@ -3,5 +3,8 @@ class Energy < ApplicationRecord
 
   validates :label, :house_id, :year, :month, :temperature, :daylight, :energy_production, presence: true
 
-  scope :chart_bubble, ->(year) { where(year: year).map { |e| [e.temperature, e.daylight, e.energy_production] } }
+  def include_energy_data(year)
+    where(year: year).map { |e| [e.temperature, e.daylight, e.energy_production] }
+  end
+
 end
