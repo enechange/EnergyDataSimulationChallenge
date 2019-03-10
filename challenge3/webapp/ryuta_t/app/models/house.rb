03d95126712edc4c1626ba1d02d6d89e53862a5f
Ryu007 <ryuta.tashiro@gmail.com>
2019-03-10 12:58:38 +0900

    Modify house.rb to add validates

--- challenge3/webapp/ryuta_t/app/models/house.rb
+++ challenge3/webapp/ryuta_t/app/models/house.rb
@@ -1,12 +1,15 @@
 class House < ApplicationRecord
   has_many :energies
 
-  scope :extract_chart_data, ->(city, year, column){where(city: city).map { |y| y.energies.where(year: year).map(&column) }}
+  validates :firstname, :lastname, :city, :num_of_people, :has_child, presence: true
+  validates :firstname, :lastname, length: { maximum: 20 }
 
-  scope :production_average, ->(city, year, column){production_sum(city, year, column) / house_amount(city, year, column)}
+  scope :extract_chart_data, ->(city, year, column) { where(city: city).map { |y| y.energies.where(year: year).map(&column) } }
 
-  scope :production_sum, ->(city, year, column){extract_chart_data(city, year, column).sum.sum}
+  scope :production_average, ->(city, year, column) { production_sum(city, year, column) / house_amount(city, year, column) }
 
-  scope :house_amount, ->(city, year, column){extract_chart_data(city, year, column).flatten!.count}
+  scope :production_sum, ->(city, year, column) { extract_chart_data(city, year, column).sum.sum }
+
+  scope :house_amount, ->(city, year, column) { extract_chart_data(city, year, column).flatten!.count }
 
 end
