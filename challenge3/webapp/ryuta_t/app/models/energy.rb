1811f6a9f293361d82bee2addd4b59274ffd3044
Ryu007 <ryuta.tashiro@gmail.com>
2019-03-10 11:45:01 +0900

    Add validates to energy.rb and Clean views

--- challenge3/webapp/ryuta_t/app/models/energy.rb
+++ challenge3/webapp/ryuta_t/app/models/energy.rb
@@ -1,5 +1,7 @@
 class Energy < ApplicationRecord
   belongs_to :house
 
+  validates :label, :house_id, :year, :month, :temperature, :daylight, :energy_production, presence: true
+
   scope :chart_bubble, ->(year) { where(year: year).map { |e| [e.temperature, e.daylight, e.energy_production] } }
 end
