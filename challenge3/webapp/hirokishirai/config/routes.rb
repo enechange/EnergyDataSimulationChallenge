561d7c33ead112cb7cd83a4fb377f70ae16b6c1e
hirokishirai <shiraihiroki.jp@gmail.com>
2019-03-17 06:54:50 +0900

    rails g controller houses index show

--- challenge3/webapp/hirokishirai/config/routes.rb
+++ challenge3/webapp/hirokishirai/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'cities/index'
   get 'cities/show'
+  get 'houses/index'
+  get 'houses/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
