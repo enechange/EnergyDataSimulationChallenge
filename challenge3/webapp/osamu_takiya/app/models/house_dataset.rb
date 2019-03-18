29f4f9ed080a0fd4e73d70f0efa4a1ca0f3accc3
Osamu Takiya <takiya@toran.sakura.ne.jp>
2019-03-19 06:06:13 +0900

    add-association-and-validation (#10)

--- challenge3/webapp/osamu_takiya/app/models/house_dataset.rb
+++ challenge3/webapp/osamu_takiya/app/models/house_dataset.rb
@@ -1,2 +1,16 @@
 class HouseDataset < ApplicationRecord
+  has_many :energy_production_datasets
+
+  validates :firstname,
+    presence: true
+  validates :lastname,
+    presence: true
+  validates :city,
+    presence: true,
+    inclusion: { in: ['London', 'Cambridge', 'Oxford'] }
+  validates :num_of_people,
+    presence: true,
+    numericality: { only_integer: true }
+  validates :has_child,
+    inclusion: { in: [true, false] }
 end
