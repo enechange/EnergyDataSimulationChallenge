f91a3e5d8cbc891636f4ff1e1d290d1f634220af
xinsky-m <xinsky.m@gmail.com>
2019-04-03 20:55:32 +0900

    db, model

---
+++ challenge3/webapp/smatsuda/edsc-c3/app/models/house.rb
@@ -0,0 +1,3 @@
+class House < ApplicationRecord
+  has_many :energy_data
+end
--- challenge3/webapp/smatsuda/edsc-c3/db/seeds.rb
+++ challenge3/webapp/smatsuda/edsc-c3/db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+def to_boolean(data)
+   res = case data
+     when 'Yes'
+       true
+     when 'No'
+       false
+     end
+   res
+end
+
+House.transaction do
+  CSV.foreach('db/house_data.csv', headers: true) do |row|
+    House.create!(
+      id:            row[0],
+      first_name:    row[1],
+      last_name:     row[2],
+      city:          row[3],
+      num_of_people: row[4],
+      has_child:     to_boolean(row[5])
+    )
+  end
+end
+
+EnergyData.transaction do
+  CSV.foreach('db/dataset_50.csv', headers: true) do |row|
+    EnergyData.create!(
+      id:                row[0],
+      label:             row[1],
+      house_id:          row[2],
+      year:              row[3],
+      month:             row[4],
+      temperature:       row[5],
+      daylight:          row[6],
+      energy_production: row[7]
+    )
+  end
+end
