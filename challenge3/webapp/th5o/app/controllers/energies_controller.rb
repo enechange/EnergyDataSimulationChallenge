cbda14b36c0dd597bc796f9457d28f3041839809
Tomoyuki Honjo <t.honjo@mstage-corp.jp>
2019-05-01 19:18:28 +0900

    move finder method into private method

--- challenge3/webapp/th5o/app/controllers/energies_controller.rb
+++ challenge3/webapp/th5o/app/controllers/energies_controller.rb
@@ -1,11 +1,17 @@
 class EnergiesController < ApplicationController
+  before_action :set_energy, only: [:show]
 
   def index
     @energies = Energy.all
   end
 
   def show
-    @energy = Energy.find(params[:id])
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_energy
+      @energy = Energy.find(params[:id])
+    end
 
 end
