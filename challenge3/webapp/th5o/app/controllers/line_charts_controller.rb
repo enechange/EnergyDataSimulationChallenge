32660b6add3eaa6a8861bb4f6b4d5d9d5496b4fe
Tomoyuki Honjo <t.honjo@mstage-corp.jp>
2019-05-02 21:42:16 +0900

    append graph segmented by family attributes

--- challenge3/webapp/th5o/app/controllers/line_charts_controller.rb
+++ challenge3/webapp/th5o/app/controllers/line_charts_controller.rb
@@ -1,26 +1,23 @@
 class LineChartsController < ApplicationController
 
   def show
-    sums = Energy.group(:year_month).sum(:energy_production).sort.to_h
-    counts = Energy.group(:year_month).count
+    whole = Energy.group(:year_month).average(:energy_production).sort.to_h
 
-    sums.each do |year_month, sum|
-      sums[year_month] = {
-        sum: sum,
-        count: counts[year_month],
-        average: sum / counts[year_month].to_f
-      }
-    end
+    @c3_columns = []
+    @c3_columns.push c3_header(whole)
+    @c3_columns.push c3_data('whole household', whole)
 
-    @c3_columns = [ c3_header(sums), c3_data(sums) ]
+    # optional data
+    ary = optional_data(params[:show_with])
+    ary.each { |data| @c3_columns.push data }
   end
 
   private
 
   # ['x', '2011-07-01', '2011-08-01', '2011-09-01', '2011-10-01', '2011-11-01', '2011-12-01']
-  def c3_header(sums)
+  def c3_header(whole)
     ary = ['x']
-    sums.keys.map do |year_month|
+    whole.keys.map do |year_month|
       str = year_month.to_s
       ary.push "#{str[0,4]}-#{str[4,2]}-01"
     end
@@ -28,10 +25,48 @@ class LineChartsController < ApplicationController
   end
 
   # ['data1', 30, 200, 100, 400, 150, 250]
-  def c3_data(sums)
-    ary = ['average']
-    sums.each do |year_month, hash|
-      ary.push hash[:average]
+  def c3_data(label, data)
+    ary = [label]
+    ary.push *data.values
+    ary
+  end
+
+  def optional_data(show_with)
+    case show_with
+    when "num_of_people"
+      energy_data_segmented_by_people
+    when "has_child"
+      energy_data_segmented_by_child
+    when "city"
+      energy_data_segmented_by_city
+    else
+      []
+    end
+  end
+
+  def energy_data_segmented_by_people
+    ary = []
+    nums = House.pluck(:num_of_people).uniq
+    nums.each do |num|
+      ary.push c3_data("n=#{num}", Energy.num_of_peoples(num).group(:year_month).average(:energy_production).sort.to_h)
+    end
+    ary
+  end
+
+  def energy_data_segmented_by_child
+    ary = []
+    bools = [true, false]
+    bools.each do |bool|
+      ary.push c3_data("has_child: #{bool}", Energy.has_children(bool).group(:year_month).average(:energy_production).sort.to_h)
+    end
+    ary
+  end
+
+  def energy_data_segmented_by_city
+    ary = []
+    cities = City.all
+    cities.each do |name, val|
+      ary.push c3_data(name.to_s, Energy.cities(val).group(:year_month).average(:energy_production).sort.to_h)
     end
     ary
   end
--- challenge3/webapp/th5o/app/models/house.rb
+++ challenge3/webapp/th5o/app/models/house.rb
@@ -4,4 +4,15 @@ class House < ApplicationRecord
 
   # attribute
   include City::Attribute
+
+  # scope
+  scope :num_of_peoples, ->(num) {
+    where(num_of_people: num)
+  }
+  scope :has_children, ->(bool) {
+    where(has_child: bool)
+  }
+  scope :cities, ->(city) {
+    where(city: city)
+  }
 end
