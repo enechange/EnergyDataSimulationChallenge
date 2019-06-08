ae1f9a643929d176b8b05779e7660941240a1ef3
make a splash <macbook@Mac-mini.local>
2019-06-08 20:38:30 +0900

    add "世帯平均日射量" line chart

--- challenge3/webapp/yomiyama/app/controllers/charts_controller.rb
+++ challenge3/webapp/yomiyama/app/controllers/charts_controller.rb
@@ -10,12 +10,26 @@ class ChartsController < ApplicationController
     @cambridge_energy_average = city_energy_average(@cambridge_house)
     @oxford_energy_average = city_energy_average(@oxford_house)
     
-    #各都市の世帯平均発電量をjson化
+    #都市ごとの世帯平均発電量をjson化
     @energy_average_json = [
       @london_energy_average,
       @cambridge_energy_average,
       @oxford_energy_average
     ].to_json
+
+    #都市ごとの平均日射量
+    @london_daylight_average = city_daylight_average(@london_house)
+    @cambridge_daylight_average = city_daylight_average(@cambridge_house)
+    @oxford_daylight_average = city_daylight_average(@oxford_house)
+
+    #都市ごとの平均日射量をjson化
+    @daylight_average_json = [
+      @london_daylight_average,
+      @cambridge_daylight_average,
+      @oxford_daylight_average
+    ].to_json
+
+    puts @energy_average_json
   end
 
   private
@@ -27,5 +41,10 @@ class ChartsController < ApplicationController
   def city_energy_average(house_ids)
     energy = house_ids.map {|house_id| (Dataset.where(house: house_id).pluck(:energyproduction).sum)}
     energy.sum / energy.size
+  end
+
+  def city_daylight_average(house_ids)
+    daylight = house_ids.map {|house_id| (Dataset.where(house: house_id).pluck(:daylight).sum)}
+    daylight.sum / daylight.size
   end
 end
