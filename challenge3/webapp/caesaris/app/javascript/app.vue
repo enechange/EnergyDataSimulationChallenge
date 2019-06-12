2b35116f4196c8de714dc64fe8d4ed333978cde3
Jerry Lee <lee@idoctor.co.jp>
2019-06-12 13:58:57 +0900

    init vue

---
+++ challenge3/webapp/caesaris/app/javascript/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+console.log('hello')
+
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
--- challenge3/webapp/caesaris/config/initializers/content_security_policy.rb
+++ challenge3/webapp/caesaris/config/initializers/content_security_policy.rb
@@ -4,6 +4,14 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
+Rails.application.config.content_security_policy do |policy|
+  if Rails.env.development?
+    policy.script_src :self, :https, :unsafe_eval
+  else
+    policy.script_src :self, :https
+  end
+end
+
 # Rails.application.config.content_security_policy do |policy|
 #   policy.default_src :self, :https
 #   policy.font_src    :self, :https, :data
