374f2f22abfd33271816cadde65655b7b244a8dd
Jerry Lee <lee@idoctor.co.jp>
2019-06-14 19:06:43 +0900

    add Component BoxCard

---
+++ challenge3/webapp/caesaris/vendor/admin/src/views/dashboard/components/BoxCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-card class="box-card-component">
+    <div
+      slot="header"
+      class="box-card-header"
+    >
+      <img src="https://picsum.photos/600/400/?random">
+    </div>
+    <div style="position:relative;">
+      AAAAA
+    </div>
+  </el-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class BoxCard extends Vue {
+
+}
+</script>
+
+<style lang="scss" >
+.box-card-component{
+  .el-card__header {
+    padding: 0px!important;
+  }
+}
+</style>
+<style lang="scss" scoped>
+.box-card-component {
+  margin-left:8px;
+
+  .box-card-header {
+    position: relative;
+    height: 220px;
+    img {
+      width: 100%;
+      height: 100%;
+      transition: all 0.2s linear;
+      &:hover {
+        transform: scale(1.1, 1.1);
+        filter: contrast(130%);
+      }
+    }
+  }
+}
+</style>
--- challenge3/webapp/caesaris/vendor/admin/src/views/dashboard/index.vue
+++ challenge3/webapp/caesaris/vendor/admin/src/views/dashboard/index.vue
@@ -9,14 +9,60 @@
         :key="role"
       >{{ role }}</span>
     </div>
+
+    <el-row :gutter="8">
+      <el-col
+        :xs="{span: 24}"
+        :sm="{span: 12}"
+        :md="{span: 12}"
+        :lg="{span: 6}"
+        :xl="{span: 6}"
+        class="card-box-col"
+      >
+        <box-card />
+      </el-col>
+      <el-col
+        :xs="{span: 24}"
+        :sm="{span: 12}"
+        :md="{span: 12}"
+        :lg="{span: 6}"
+        :xl="{span: 6}"
+        class="card-box-col"
+      >
+        <box-card />
+      </el-col>
+      <el-col
+        :xs="{span: 24}"
+        :sm="{span: 12}"
+        :md="{span: 12}"
+        :lg="{span: 6}"
+        :xl="{span: 6}"
+        class="card-box-col"
+      >
+        <box-card />
+      </el-col>
+      <el-col
+        :xs="{span: 24}"
+        :sm="{span: 12}"
+        :md="{span: 12}"
+        :lg="{span: 6}"
+        :xl="{span: 6}"
+        class="card-box-col"
+      >
+        <box-card />
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import BoxCard from './components/BoxCard.vue'
 import { UserModule } from '@/store/modules/user'
 
-@Component
+@Component({
+  components: { BoxCard }
+})
 export default class Dashboard extends Vue {
   get name() {
     return UserModule.name
@@ -31,12 +77,20 @@ export default class Dashboard extends Vue {
 <style lang="scss" scoped>
 .dashboard {
   &-container {
-    margin: 30px;
+    // margin: 30px;
+    padding: 32px;
+    background-color: rgb(240, 242, 245);
+    position: relative;
+    min-height: calc(100vh - 50px);
   }
 
   &-text {
     font-size: 30px;
     line-height: 46px;
   }
+}
+
+.card-box-col {
+  margin-bottom:30px;
 }
 </style>
