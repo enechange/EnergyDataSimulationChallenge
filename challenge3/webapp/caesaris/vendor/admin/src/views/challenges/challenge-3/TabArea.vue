ceea41dc234c32393fc12b5b5ff0c5c26b099414
Jerry Lee <lee@idoctor.co.jp>
2019-06-17 16:32:39 +0900

    add tab draft

---
+++ challenge3/webapp/caesaris/vendor/admin/src/views/challenges/challenge-3/TabArea.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  el-tabs(v-model='activeName', @tab-click='handleClick')
+    el-tab-pane(label='用户管理', name='first') 用户管理
+    el-tab-pane(label='配置管理', name='second') 配置管理
+    el-tab-pane(label='角色管理', name='third') 角色管理
+    el-tab-pane(label='定时任务补偿', name='fourth') 定时任务补偿
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import { Component as C } from 'vue'
+
+@Component
+export default class TabArea extends Vue {
+  activeName: string = 'second'
+
+  handleClick(tab: C, event: MouseEvent) {
+    console.log(tab.name, event)
+  }
+}
+</script>
--- challenge3/webapp/caesaris/vendor/admin/src/views/challenges/challenge-3/index.vue
+++ challenge3/webapp/caesaris/vendor/admin/src/views/challenges/challenge-3/index.vue
@@ -1,8 +1,36 @@
 <template lang="pug">
-  div(style='padding:30px;')
-    el-alert(:closable='false', title='Challenge 3')
+  div.contents-area(style='padding:30px;')
+    h1.challenge-ttl Challenge 3
+    p.challenge-txt Web Application
+    //el-alert(:closable='false', title='Challenge 3')
+    <tab-area />
 </template>
 
 <script lang="ts">
-export default {}
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import TabArea from './TabArea.vue'
+
+@Component({
+  components: { TabArea }
+})
+export default class Challenge3 extends Vue {
+
+}
 </script>
+
+<style lang="scss" scoped>
+.contents-area {
+  padding:30px;
+  .challenge-ttl {
+    font-size: 1.5em;
+    margin-top: 0;
+    margin-bottom: 10px;
+    color: #333;
+  }
+
+  .challenge-txt {
+    margin-top: 10px;
+    color: #333;
+  }
+}
+</style>
