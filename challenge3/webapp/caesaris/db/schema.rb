ebb97c4e5d2c7b4abe724fd96fa0f3fa136c07e8
Jerry Lee <lee@idoctor.co.jp>
2019-06-19 15:53:23 +0900

    add model user

--- challenge3/webapp/caesaris/db/schema.rb
+++ challenge3/webapp/caesaris/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_075751) do
+ActiveRecord::Schema.define(version: 2019_06_19_054146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,18 @@ ActiveRecord::Schema.define(version: 2019_06_12_075751) do
     t.index ["has_child"], name: "index_houses_on_has_child"
     t.index ["lastname"], name: "index_houses_on_lastname"
     t.index ["num_of_people"], name: "index_houses_on_num_of_people"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
