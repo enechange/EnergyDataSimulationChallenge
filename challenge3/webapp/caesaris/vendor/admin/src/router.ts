285bcc888dfcfa425becf236c9538b21a01d8977
Jerry Lee <lee@idoctor.co.jp>
2019-06-20 21:11:59 +0900

    remove unused codes

--- challenge3/webapp/caesaris/vendor/admin/src/router.ts
+++ challenge3/webapp/caesaris/vendor/admin/src/router.ts
@@ -94,61 +94,6 @@ export default new Router({
       ]
     },
     {
-      path: '/nested',
-      component: Layout,
-      redirect: '/nested/menu1',
-      name: 'Nested',
-      meta: { title: 'Nested', icon: 'nested' },
-      children: [
-        {
-          path: 'menu1',
-          component: () => import(/* webpackChunkName: "menu1" */ '@/views/nested/menu1/index.vue'),
-          name: 'Menu1',
-          meta: { title: 'Menu1' },
-          children: [
-            {
-              path: 'menu1-1',
-              component: () => import(/* webpackChunkName: "menu1-1" */ '@/views/nested/menu1/menu1-1/index.vue'),
-              name: 'Menu1-1',
-              meta: { title: 'Menu1-1' }
-            },
-            {
-              path: 'menu1-2',
-              component: () => import(/* webpackChunkName: "menu1-2" */ '@/views/nested/menu1/menu1-2/index.vue'),
-              name: 'Menu1-2',
-              meta: { title: 'Menu1-2' },
-              children: [
-                {
-                  path: 'menu1-2-1',
-                  component: () => import(/* webpackChunkName: "menu1-2-1" */ '@/views/nested/menu1/menu1-2/menu1-2-1/index.vue'),
-                  name: 'Menu1-2-1',
-                  meta: { title: 'Menu1-2-1' }
-                },
-                {
-                  path: 'menu1-2-2',
-                  component: () => import(/* webpackChunkName: "menu1-2-2" */ '@/views/nested/menu1/menu1-2/menu1-2-2/index.vue'),
-                  name: 'Menu1-2-2',
-                  meta: { title: 'Menu1-2-2' }
-                }
-              ]
-            },
-            {
-              path: 'menu1-3',
-              component: () => import(/* webpackChunkName: "menu1-3" */ '@/views/nested/menu1/menu1-3/index.vue'),
-              name: 'Menu1-3',
-              meta: { title: 'Menu1-3' }
-            }
-          ]
-        },
-        {
-          path: 'menu2',
-          component: () => import(/* webpackChunkName: "menu2" */ '@/views/nested/menu2/index.vue'),
-          name: 'Menu2',
-          meta: { title: 'Menu2' }
-        }
-      ]
-    },
-    {
       path: 'external-link',
       component: Layout,
       children: [
--- challenge3/webapp/caesaris/vendor/admin/src/utils/validate.ts
+++ challenge3/webapp/caesaris/vendor/admin/src/utils/validate.ts
@@ -1,5 +1,3 @@
-/* eslint-disable comma-dangle */
-// export const isValidUsername = (str: string) => ['admin', 'editor'].indexOf(str.trim()) >= 0
 const emailRegExp = /^([\w+-]\.?)+@[\w\d-]+(\.[\w\d-]+)*\.[a-z]+$/i
 export const isValidUsername = (str: string) => emailRegExp.test(str)
 
