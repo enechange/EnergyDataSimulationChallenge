825d739e95c1eed57ada9331460e4b62f7766bfe
Jerry Lee <lee@idoctor.co.jp>
2019-06-20 20:19:16 +0900

    authenticate_user for graphql

--- challenge3/webapp/caesaris/app/controllers/graphql_controller.rb
+++ challenge3/webapp/caesaris/app/controllers/graphql_controller.rb
@@ -1,5 +1,6 @@
 class GraphqlController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
 
   def execute
     variables = ensure_hash(params[:variables])
