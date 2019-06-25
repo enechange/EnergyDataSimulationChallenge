4387bb2e508951f635be9ba08e4ecbab94a56605
yukaina <misaki.yukaina@gmail.com>
2019-06-25 13:06:53 +0900

    add validates, scope
    presence:
      City.name
      Family.first_name, Family.last_name, Family.num_of_people
      MonthlyEnergyLog.temperature, MonthlyEnergyLog.daylight, MonthlyEnergyLog.production_volume
      MonthlyLabel.year, MonthlyLabel.month
    uniqueness:
      City.name
      MonthlyLabel.[year, month]
    numericality:
      Family.num_of_people
      MonthlyEnergyLog.temperature, MonthlyEnergyLog.daylight, MonthlyEnergyLog.production_volume
    inclusion:
      Family.has_child
    
    scope:
      MonthlyEnergyLog.group_by_monthly_label

--- challenge3/webapp/yukaina/app/models/city.rb
+++ challenge3/webapp/yukaina/app/models/city.rb
@@ -1,2 +1,3 @@
 class City < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
 end
--- challenge3/webapp/yukaina/app/models/family.rb
+++ challenge3/webapp/yukaina/app/models/family.rb
@@ -1,2 +1,9 @@
 class Family < ApplicationRecord
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+    validates :num_of_people
+  end
+  validates :num_of_people, numericality: true
+  validates :has_child, inclusion: { in: [true, false] }
 end
