15aaf948ef1da4a34187194e251167437b69520c
yukaina <misaki.yukaina@gmail.com>
2019-06-25 17:40:40 +0900

    Suppress rubocop warnings

--- challenge3/webapp/yukaina/app/models/monthly_energy_log.rb
+++ challenge3/webapp/yukaina/app/models/monthly_energy_log.rb
@@ -2,11 +2,10 @@ class MonthlyEnergyLog < ApplicationRecord
   belongs_to :monthly_label
   belongs_to :house
   belongs_to :family
-
-  with_options presence: true, numericality: true  do
+  with_options presence: true, numericality: true do
     validates :temperature
     validates :daylight
     validates :production_volume
   end
-  scope :group_by_monthly_label, ->{ group(:monthly_label_id) }
+  scope :group_by_monthly_label, -> { group(:monthly_label_id) }
 end
