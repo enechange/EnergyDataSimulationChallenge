2502d7286ff342b6ce387dd1482bfebbf94eb94a
yukaina <misaki.yukaina@gmail.com>
2019-06-25 23:52:27 +0900

    add rspec-rails, factory_bot_rails, rails generate rspec:install

--- challenge3/webapp/yukaina/config/application.rb
+++ challenge3/webapp/yukaina/config/application.rb
@@ -32,5 +32,12 @@ module Yukaina
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        controller_specs: false,
+        routing_specs: false
+    end
   end
 end
