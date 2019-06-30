700fc3518dd8bff9962a4339ba89a1124c8a06ce
Jerry Lee <lee@idoctor.co.jp>
2019-06-30 13:40:17 +0900

    delegate => has_one

--- challenge3/webapp/caesaris/app/models/dataset.rb
+++ challenge3/webapp/caesaris/app/models/dataset.rb
@@ -1,6 +1,6 @@
 class Dataset < ApplicationRecord
   belongs_to :house
-  delegate :city, to: :house
+  has_one :city, through: :house
   validates_presence_of :label, :year, :month, :temperature, :daylight, :energy_production
 
   scope :order_by_date, -> { order(:year, :month) }
