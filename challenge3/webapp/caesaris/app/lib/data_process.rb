b06e74ae7c906e6ea620709db051cebc3b138360
Jerry Lee <lee@idoctor.co.jp>
2019-07-01 14:59:39 +0900

    update person_energy_prod_time_series

--- challenge3/webapp/caesaris/app/lib/data_process.rb
+++ challenge3/webapp/caesaris/app/lib/data_process.rb
@@ -7,6 +7,7 @@ class DataProcess
       max = en_prods.maximum.as('max')
       min = en_prods.minimum.as('min')
       avg = en_prods.average.as('avg')
+
       City.all.each_with_object({}) do |city, results|
         results[city.name] = city.datasets
           .select(:year, :month, max, min, avg)
@@ -24,26 +25,32 @@ class DataProcess
 
     # calc average energy_production per person for cities
     def person_energy_prod_time_series
-      results = {}
-      City.all.each do |city|
-        res_ttl = {}
-        res_ave = {}
-        city.houses.each do |house|
-          house.datasets.order(:year, :month).each do |dataset|
-            res_ttl[dataset.date_str] = [] if res_ttl[dataset.date_str].nil?
-            res_ttl[dataset.date_str] << dataset.energy_production / house.num_of_people.to_f
-          end
-          res_ttl.each do |date_str, en_prods|
-            res_ave[date_str] = [
-              en_prods.min,
-              en_prods.sum / en_prods.size.to_f,
-              en_prods.max
+      max = Arel.sql('MAX(per_person)').as('max')
+      min = Arel.sql('MIN(per_person)').as('min')
+      avg = Arel.sql('AVG(per_person)').as('avg')
+
+      City.all.each_with_object({}) do |city, results|
+        sql = <<-SQL
+          (SELECT year, month,
+            (CAST(energy_production AS FLOAT) / CAST(num_of_people AS FLOAT)) AS per_person
+          FROM datasets, houses
+          WHERE "datasets"."house_id" = "houses"."id" AND "houses"."city_id" = #{city.id})
+        SQL
+
+        arel = Arel::Table.new(nil).project(:year, :month, max, min, avg)
+          .from(Arel.sql(sql).as('datasets_per_person'))
+          .group(:year, :month).order(:year, :month)
+        con = ActiveRecord::Base.connection
+        results[city.name] = con.select_all(arel)
+          .to_hash.each_with_object({}) do |sql_res, eng_prod_list|
+            date_str = DataProcess.date_str(sql_res['year'], sql_res['month'])
+            eng_prod_list[date_str] = [
+              sql_res['min'],
+              sql_res['avg'],
+              sql_res['max'],
             ]
           end
-        end
-        results[city.name] = res_ave
       end
-      results
     end
 
     # key: [:daylight, :temperature]
