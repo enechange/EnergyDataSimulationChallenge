2ce82e2ba8bf72ec890d3e567cbf979182d03adf
Jerry Lee <lee@idoctor.co.jp>
2019-07-11 16:22:00 +0900

    update Echarts Playground draft

--- challenge3/webapp/caesaris/vendor/admin/src/views/echarts/index.vue
+++ challenge3/webapp/caesaris/vendor/admin/src/views/echarts/index.vue
@@ -1,8 +1,20 @@
 <template lang="pug">
-  div(style='padding:30px;')
-    el-alert(:closable='false', title='Echarts Playground Is Under Construction.')
+  div.contents-area(style='padding:30px;')
+    h1.challenge-ttl Echarts Playground
+    p.challenge-txt Visualization of Given Data
+    el-tabs(v-model='activeName')
+      el-tab-pane.tab-panel(label='Histogram', name='histogram', lazy=true)
 </template>
 
 <script lang="ts">
-export default {}
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class EchartsPlayground extends Vue {
+  private activeName: string = 'histogram'
+}
 </script>
+
+<style lang="scss" scoped>
+@import "src/styles/challenges.scss";
+</style>
