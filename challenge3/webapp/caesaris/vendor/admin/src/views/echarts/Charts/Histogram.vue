71dd7dc74cbf537d7bffd407cfa13c93d86b0bef
Jerry Lee <lee@idoctor.co.jp>
2019-07-11 21:41:29 +0900

    update Histogram draft

--- challenge3/webapp/caesaris/vendor/admin/src/views/echarts/Charts/Histogram.vue
+++ challenge3/webapp/caesaris/vendor/admin/src/views/echarts/Charts/Histogram.vue
@@ -1,25 +1,38 @@
 <template lang="pug">
   section
+    input-dialog(
+      :on-open-flg='openDialogFlg', @data-change='onDataChange',
+      placeholder='0.23\n0.13\n0.12',
+    )
     h3.chart-ttl
       |Histogram According to Square-root Choice
-      el-button.input-data-btn(type='text', icon='el-icon-s-data')
-        | Input Data!
-    .chart(:id="id", style="width: 100%;height: 450px;")
+      el-button.input-data-btn(
+        @click='openDataInputDialog()',
+        type='text', icon='el-icon-s-data'
+      ) Input Data!
+    .chart(:id='id', style='width: 100%;height: 450px;')
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
+import InputDialog from './InputDialog.vue'
 import * as echarts from 'echarts'
 
-@Component
+@Component({
+  components: { InputDialog },
+})
 export default class Histogram extends Vue {
   @Prop({ default: 'histogram-playground' }) private id!: string
   private chart: echarts.ECharts | null = null
   private chartData: string | null = null
+  private openDialogFlg = 0
+
+  openDataInputDialog() {
+    this.openDialogFlg = Date.now()
+  }
 
   mounted() {
     this.initChart()
-    this.updateChart()
   }
 
   beforeDestroy() {
@@ -36,10 +49,14 @@ export default class Histogram extends Vue {
     this.chart.showLoading()
   }
 
-  @Watch('chartData')
-  onValueChange(newValue: string, oldValue: string): void {
+  onDataChange(data: string) {
+    console.log('dataChange', data)
     this.updateChart()
   }
+  // @Watch('chartData')
+  // onValueChange(newValue: string, oldValue: string): void {
+  //   this.updateChart()
+  // }
 
   updateChart() {
     if (this.chart) {
