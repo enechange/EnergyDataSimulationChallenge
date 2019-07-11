53b3aa9a8c50e7013db9ca6562a5fbb3b3a6ac4c
Jerry Lee <lee@idoctor.co.jp>
2019-07-11 15:26:13 +0900

    add echarts-playground draft

---
+++ challenge3/webapp/caesaris/vendor/admin/src/views/echarts/index.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+  div(style='padding:30px;')
+    el-alert(:closable='false', title='Echarts Playground Is Under Construction.')
+</template>
+
+<script lang="ts">
+export default {}
+</script>
