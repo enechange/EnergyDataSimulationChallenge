05c4a30090410363549ab138f855ddd90f486d08
kagami ryohei <a7112069@gmail.com>
2019-07-28 22:22:09 +0900

    heroku

--- challenge3/webapp/kagami/config/environments/production.rb
+++ challenge3/webapp/kagami/config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.initialize_on_precompile=false
 end
