5071281eb17f5f15e9d34da6773ba175a39de28f
M S <doll.rika01@gmail.com>
2019-07-29 14:57:19 +0900

    add schema

--- challenge3/webapp/msaito/app/models/dataset.rb
+++ challenge3/webapp/msaito/app/models/dataset.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: datasets
+#
+#  id                :bigint           not null, primary key
+#  label             :integer
+#  house_id          :bigint           not null
+#  year              :integer
+#  month             :integer
+#  temperature       :decimal(10, )
+#  daylight          :decimal(10, )
+#  energy_production :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 class Dataset < ApplicationRecord
 end
--- challenge3/webapp/msaito/app/models/house.rb
+++ challenge3/webapp/msaito/app/models/house.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id            :bigint           not null, primary key
+#  firstname     :string(255)
+#  lastname      :string(255)
+#  city          :string(255)
+#  num_of_people :integer
+#  has_child     :string(255)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class House < ApplicationRecord
 end
