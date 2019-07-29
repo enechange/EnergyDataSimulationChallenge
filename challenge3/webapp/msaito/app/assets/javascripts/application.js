2e775686ce1c01989e384def2ec01c00872788dc
M S <doll.rika01@gmail.com>
2019-07-29 15:06:58 +0900

    setting chart.js

--- challenge3/webapp/msaito/app/assets/javascripts/application.js
+++ challenge3/webapp/msaito/app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require Chart.min
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
