187e68ae387b2c5cbb094ef837573644b01d2b1a
massy-o <telitos000@gmail.com>
2019-10-25 08:31:32 +0900

    クロスオリジン対策

--- challenge3/webapp/masuda/web/config/application.rb
+++ challenge3/webapp/masuda/web/config/application.rb
@@ -29,7 +29,15 @@ module Web
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    # Permit cross origin
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :options, :head]
+      end
+    end
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
