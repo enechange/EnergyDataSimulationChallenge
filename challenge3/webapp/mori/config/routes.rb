e7f22bddf0661ed9ae125d3f643b452e83c782c7
msymori <msy.mori.developer@gmail.com>
2019-11-10 02:07:54 +0000

    DATAをDBに追加

--- challenge3/webapp/mori/config/routes.rb
+++ challenge3/webapp/mori/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+    root to: 'datas#index'
+    resources :datas, only: [:index, :show]
 end
--- challenge3/webapp/mori/db/seeds.rb
+++ challenge3/webapp/mori/db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach('db/house_data.csv', headers: true) do |row|
+    House.create(
+        firstname: row[1],
+        lastname: row[2],
+        city: row[3],
+        num_of_people: row[4],
+        has_child: row[5]
+        )
+end 
+
+CSV.foreach('db/dataset_50.csv', headers: true) do |row|
+    Dataset.create(
+        label: row[1],
+        house_id: row[2],
+        year: row[3],
+        month: row[4],
+        temperature: row[5],
+        daylight: row[6],
+        energy_production: row[7]
+        )
+end
