4ef27ffda28532a66cfee4c42b66c5c877d42f2f
Yukio Ugajin <jinugasachio@gmail.com>
2019-11-20 20:17:05 +0900

    add chart-1.js & chart-2.js to javascript/packs

---
+++ challenge3/webapp/yukio_ugajin/app/javascript/packs/chart-2.js
@@ -0,0 +1,20 @@
+const ctx = document.getElementById('myChart2').getContext('2d');
+// debugger;
+const chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: 'line',
+
+  // The data for our dataset
+  data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45]
+      }]
+  },
+
+  // Configuration options go here
+  options: {}
+});
