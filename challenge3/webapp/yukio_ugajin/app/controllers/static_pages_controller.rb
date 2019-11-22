bdc49d02c574cc21484de8ec8b9b70435b653826
Yukio Ugajin <jinugasachio@gmail.com>
2019-11-22 23:55:15 +0900

    add association and validation to energy_detail model

--- challenge3/webapp/yukio_ugajin/app/controllers/static_pages_controller.rb
+++ challenge3/webapp/yukio_ugajin/app/controllers/static_pages_controller.rb
@@ -6,17 +6,29 @@ class StaticPagesController < ApplicationController
     EnergyDetail.all.each do |e|
       data << e.scatter_position
     end
-    @all_energy = scatter_params('All Data', '#ffea00', '#ffbb00', data)
+    @all_energy = [scatter_params('All Data', '#ffea00', '#ffbb00', data)]
+  end
+
+  def city
+    cambridge = []
+    london = []
+    oxford = []
+
+    EnergyDetail.all.each do |e|
+      
+    end
+
+
   end
 
   private
 
     def scatter_params(label, background, border, data)
-      [{
+      {
         label: label,
         backgroundColor: background,
         borderColor: border,
         data: data
-      }]
+      }
     end
 end
--- challenge3/webapp/yukio_ugajin/app/models/energy_detail.rb
+++ challenge3/webapp/yukio_ugajin/app/models/energy_detail.rb
@@ -1,4 +1,21 @@
 class EnergyDetail < ApplicationRecord
+  # Association
+  belongs_to :house
+
+  # Validation
+  validates :label, 
+            presence: true, numericality: { only_integer: true }
+  validates :year,
+            presence: true, numericality: { only_integer: true }
+  validates :month,
+            presence: true, numericality: { only_integer: true }
+  validates :temperture,
+            presence: true, numericality: true
+  validates :daylight,
+            presence: true, numericality: true
+  validates :energy_production,
+            presence: true, numericality: { only_integer: true }
+
   def scatter_position
     { x: daylight, y: energy_production }
   end
