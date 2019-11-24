277615951507a1c0bcc9a685850d4fd347151d64
Yukio Ugajin <jinugasachio@gmail.com>
2019-11-24 13:37:08 +0900

    change chart.js

--- challenge3/webapp/yukio_ugajin/app/javascript/packs/chart.js
+++ challenge3/webapp/yukio_ugajin/app/javascript/packs/chart.js
@@ -1,6 +1,6 @@
 const select_box = document.getElementById('select_box');
 
-let get_data = (url) => {
+const get_data = (url) => {
   axios.get('/' + url )
     .then(function (response) {
       scatter_chart(response.data);
@@ -35,8 +35,42 @@ const scatter_chart = (dataArray) => {
   })
 };
 
+const line_chart = () => {
+  const ctx = document.getElementById('myChart2').getContext('2d');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: '#FFEA00',
+            borderColor: '#FFBB00',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    options: {
+      scales: {
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Daylight'
+          }
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Energy Production'
+          }
+        }]
+      }
+    }
+  });
+};
+
 window.onload = () => {
   get_data('all');
+  line_chart();
 };
 
 select_box.addEventListener('change', (e) => {
