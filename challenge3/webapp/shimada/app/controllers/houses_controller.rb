cc7578c376f248c8d24d1dffe266438763efafd7
shimadama <yuhki.shimada@gmail.com>
2019-11-25 00:31:18 +0900

    houses一覧画面を作成

--- challenge3/webapp/shimada/app/controllers/houses_controller.rb
+++ challenge3/webapp/shimada/app/controllers/houses_controller.rb
@@ -1,4 +1,20 @@
 class HousesController < ApplicationController
+
   def index
+    @selected_city     = params[:city] ||= 'London'
+    @energy_each_years = Energy.energy_each_years(@selected_city)
+
+    @children_by_city = House.children_by_city
+
+    house_columns = House::PLUCK_KEYS
+    @houses       = House.pluck_to_hash(house_columns)
+  end
+
+  private
+
+  def house_params
+    params.require(:house).permit(
+      :city
+    )
   end
 end
--- challenge3/webapp/shimada/app/models/house.rb
+++ challenge3/webapp/shimada/app/models/house.rb
@@ -1,16 +1,23 @@
 class House < ApplicationRecord
+  include PluckToHash
 
   CITIES     = %w[London Cambridge Oxford].freeze
+  PLUCK_KEYS = %i[id firstname lastname city num_of_people has_child].freeze
 
   has_many :energies
 
   validates :firstname, :lastname, :city, :num_of_people, presence: true
-  validates :city, inclusion: { in: CITIES }
+  validates :city,      inclusion: { in: CITIES }
   validates :has_child, inclusion: { in: [true, false] }
+
+  scope :with_energies,          -> { includes(:energies) }
+  scope :group_by_city,          -> (city_name) { group(city_name) }
+  scope :by_city,                -> (city_name) { where(city: city_name) }
+  scope :count_children_by_city, -> (city_name) { by_city(city_name).group(:has_child).count }
 
   class << self
     def csv_header_converters
-      lambda { |name|
+      -> (name) {
         {
           ID:            :id,
           Firstname:     :firstname,
@@ -30,6 +37,19 @@ class House < ApplicationRecord
           field
         end
       }
+    end
+
+    def target_cities
+      pluck(:city).uniq
+    end
+
+    def children_by_city
+      CITIES.map do |city_name|
+        {
+          name: city_name,
+          data: count_children_by_city(city_name)
+        }
+      end
     end
   end
 end
