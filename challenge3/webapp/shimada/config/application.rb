16c45ac7e574ce1d749eee365d3b0f3da87925d4
shimadama <yuhki.shimada@gmail.com>
2019-11-24 23:49:07 +0900

    初期設定を変更

--- challenge3/webapp/shimada/config/application.rb
+++ challenge3/webapp/shimada/config/application.rb
@@ -19,15 +19,17 @@ Bundler.require(*Rails.groups)
 
 module Shimada
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone                      = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale            = :ja
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+      g.system_tests false
+      g.template_engine :slim
+    end
   end
 end
