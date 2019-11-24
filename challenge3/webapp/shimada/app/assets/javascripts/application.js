e44cdaa29fad271c0af2d7dbc0544dbdf033c6ad
shimadama <yuhki.shimada@gmail.com>
2019-11-25 00:26:08 +0900

    admin-lteとhighchartsを追加

--- challenge3/webapp/shimada/app/assets/javascripts/application.js
+++ challenge3/webapp/shimada/app/assets/javascripts/application.js
@@ -12,5 +12,14 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+//= require chartkick
+//= require highcharts
+//= require admin-lte/plugins/jquery/jquery.min
+//= require admin-lte/plugins/bootstrap/js/bootstrap.bundle.min
+//= require admin-lte/dist/js/adminlte.js
+//= require admin-lte//plugins/datatables/jquery.dataTables
+//= require admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4
+//= require data_tables
+//= require admin-lte/dist/js/demo
+//= require admin-lte/dist/js/pages/dashboard3
 //= require_tree .
