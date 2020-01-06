2571b6985d19732e54253c5eae279280a6f6fa67
Shaw <hamkuu@gmail.com>
2020-01-06 19:48:53 +0900

    :fire: remove REST API endpoints

--- challenge3/webapp/hamkuu/backend_rails_api/config/routes.rb
+++ challenge3/webapp/hamkuu/backend_rails_api/config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
-
-  namespace :api do
-    namespace :v1 do
-      get 'energy-data/house-owners', action: :index, controller: 'house_owners'
-      get 'energy-data/houses', action: :index, controller: 'houses'
-      get 'energy-data/household-energy-records', action: :index, controller: 'household_energy_records'
-    end
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
