2dec224cebd2ab7089229625435c11e9a5790fd5
eggchi21 <eggchi21@yahoo.co.jp>
2020-02-02 21:28:49 +0900

    add deploy

--- challenge3/webapp/keita_eguchi/config/application.rb
+++ challenge3/webapp/keita_eguchi/config/application.rb
@@ -10,7 +10,7 @@ module KeitaEguchi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
