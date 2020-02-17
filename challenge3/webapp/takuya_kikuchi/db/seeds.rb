b30e47344c4ab8770cbd372470bc4d142a2e0d81
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-02-17 18:35:52 +0900

    WIP db_seed

--- challenge3/webapp/takuya_kikuchi/db/seeds.rb
+++ challenge3/webapp/takuya_kikuchi/db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+
+filepath    = 'data/house_data.csv'
+puts 'Creating house data...'
+CSV.foreach(filepath, csv_options) do |row|
+  Dataset.create!(
+    first_name: row["Firstname"],
+    last_name: row["Lastname"],
+    city: row["City"],
+    num_of_people: row["num_of_people"].to_i,
+    has_child: row["has_child"] == true
+  )
+end
+
+filepath    = 'data/dataset_50.csv'
+puts 'Creating datasets...'
+CSV.foreach(filepath, csv_options) do |row|
+  Dataset.create!(
+    label: row["Label"].to_i,
+    house_data_id: row["house"].to_i,
+    year: row["Year"].to_i,
+    month: row["Month"].to_i,
+    temperature: row["Temperature"].to_f,
+    daylight: row["Daylight"].to_f,
+    energy_production: row["EnergyProduction"].to_i
+  )
+end
