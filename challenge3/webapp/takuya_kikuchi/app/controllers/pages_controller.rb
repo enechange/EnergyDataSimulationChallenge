faedc3031dda0e553ca85e2cd9b4469743ce04e8
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-02-20 18:52:41 +0900

    added temperature and daylight

--- challenge3/webapp/takuya_kikuchi/app/controllers/pages_controller.rb
+++ challenge3/webapp/takuya_kikuchi/app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
   def main
-    year_month_groups = Dataset.group(:year,:month).sum(:energy_production)
+    year_month_energy = Dataset.group(:year,:month).sum(:energy_production)
+    year_month_temperature = Dataset.group(:year,:month).average(:temperature)
+    year_month_daylight = Dataset.group(:year,:month).average(:daylight)
     # Bar-chart x-axis
-    gon.year_months = year_month_groups.keys.sort
+    gon.year_months = year_month_energy.keys.sort
     # Bar-chart y-axis
-    gon.production = year_month_groups.values
+    gon.production = year_month_energy.values
+    gon.temperature = year_month_temperature.values
+    gon.daylight = year_month_daylight.values
   end
 end
--- challenge3/webapp/takuya_kikuchi/app/javascript/packs/pages/main.js
+++ challenge3/webapp/takuya_kikuchi/app/javascript/packs/pages/main.js
@@ -1,37 +1,49 @@
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: gon.year_months,
-        datasets: [{
-            label: 'Energy production of House 1',
-            data: gon.production,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+  type: 'bar',
+  data: {
+    labels: gon.year_months,
+    datasets: [{
+      label: 'Energy production',
+      data: gon.production,
+      backgroundColor: 'rgba(0, 153, 0, 0.5)',
+      borderColor: 'rgba(0, 153, 0, 0.8)',
+      borderWidth: 1,
+      yAxisID: "y-axis-left"
+    }, {
+      label: 'Temperature',
+      data: gon.temperature,
+      backgroundColor: 'rgba(255, 204, 255, 0.5)',
+      borderColor: 'rgba(255, 204, 255, 0.5)',
+      type: 'line',
+      yAxisID: "y-axis-right-1"
+    }, {
+      label: 'Day light',
+      data: gon.daylight,
+      backgroundColor: 'rgba(255, 255, 153, 0.5)',
+      borderColor: 'rgba(255, 255, 153, 0.5)',
+      type: 'line',
+      yAxisID: "y-axis-right-2"
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+        id: "y-axis-left",
+        position: "left",
+          ticks: {
+            beginAtZero: true
+          }
+        },
+        {
+          id: "y-axis-right-1",
+          position: "right"
+        },
+        {
+          id: "y-axis-right-2",
+          position: "right"
+        }],
     }
+  }
 });
