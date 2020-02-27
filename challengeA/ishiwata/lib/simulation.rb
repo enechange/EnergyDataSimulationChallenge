f822f2692335a67c7e2a6d137cd78ed461b92263
石渡拓海 <ishiwatatakumi@ishiwatatakuminoMacBook-Pro.local>
2020-02-27 19:05:01 +0900

    add simulate method to 2 plan

--- challengeA/ishiwata/lib/simulation.rb
+++ challengeA/ishiwata/lib/simulation.rb
@@ -10,7 +10,52 @@ class Simulation
 
 
   def simulate
+    result = []
+
+    result.push(use_tokyoenergy_plan)
+    result.push(use_loop_plan)
+
+    result
+  end
+
+  def use_tokyoenergy_plan
+    base_pay =
+      case @ampere
+      when 10
+        286
+      when 15
+        429
+      when 20
+        572
+      when 30
+        858
+      when 40
+        1144
+      when 50
+        1430
+      when 60
+        1716
+      end
+
+    add_pay =
+      if @usage > 300
+        30.57 * (@usage - 300 ) + 26.48 * 180 + 26.48 + 19.88 * 120
+      elsif @usage > 120
+        26.48 * (@usage - 120) + 19.88 * 120
+      else
+        19.88 * @usage
+      end
+
+    (base_pay + add_pay).floor
 
   end
+
+  def use_loop_plan
+
+    (@usage * 26.40).floor
+
+  end
+
+
 
 end
--- challengeA/ishiwata/spec/lib/simulation.rb
+++ challengeA/ishiwata/spec/lib/simulation.rb
@@ -10,4 +10,13 @@ describe "Simulation" do
     expect(simulation.ampere).to eq(40)
     expect(simulation.usage).to eq(280)
   end
+
+  it "is simulated" do
+
+    simulation = Simulation.new(40, 280)
+
+    expect(simulation.simulate).to eq([7766,7392,])
+  end
+
+
 end
