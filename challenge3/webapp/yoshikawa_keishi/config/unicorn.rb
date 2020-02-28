9a444a1bc6b8c3248bdf06b8d252a62f6bfe6991
yoshikawa keishi <a130809k@gmail.com>
2020-02-28 11:17:32 +0900

    set unicorn

---
+++ challenge3/webapp/yoshikawa_keishi/config/unicorn.rb
@@ -0,0 +1,45 @@
+app_path = File.expand_path('../../', __FILE__)
+
+worker_processes 1
+
+working_directory app_path
+
+pid "#{app_path}/tmp/pids/unicorn.pid"
+
+listen 3000
+
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+timeout 60
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+run_once = true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+
+  if run_once
+    run_once = false 
+  end
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
+    end
+  end
+end
+
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+end
