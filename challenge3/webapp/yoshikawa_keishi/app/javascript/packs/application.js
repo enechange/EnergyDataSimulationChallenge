1e3518658be58899dec8f65d4f229576ec09f8fa
yoshikawa keishi <a130809k@gmail.com>
2020-02-28 10:11:20 +0900

    update style

--- challenge3/webapp/yoshikawa_keishi/app/javascript/packs/application.js
+++ challenge3/webapp/yoshikawa_keishi/app/javascript/packs/application.js
@@ -7,7 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("chartkick") 
+require("chart.js")
+import 'bootstrap-material-design'
+import '../stylesheets/application'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
