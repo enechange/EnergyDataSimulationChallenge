4c5626523af61f40e61148e4f32fa2d901119492
yoshikawa keishi <a130809k@gmail.com>
2020-03-25 12:26:44 +0900

    make logic clear and extendable

---
+++ challengeA/yoshikawa_keishi/lib/simulator.rb
@@ -0,0 +1,97 @@
+class Simulator 
+  attr_reader :amp, :usage
+  @@EP_planB_SET = {rate: {0 => 19.88, 120 => 26.48, 300 => 30.57}, basicChargeRate: 28.6 }
+  @@TG_ZUTTOMO_SET = {rate: {0 => 23.67, 140 => 23.88, 350 => 26.41}, basicChargeRate: 28.6 }
+  @@acceptableAmperes = [10, 15, 20, 30, 40, 50, 60]
+
+  def initialize(amp, usage)
+    @amp, @usage = amp, usage
+  end
+
+ # 価格の安い順に各プランを表示
+  def simulate
+    if @@acceptableAmperes.include?(amp) && usage.is_a?(Integer) 
+      output = [
+        {provider_name: "東京電力エナジーパートナー", plan_name: "従量電灯B", price: "#{ep_planB}"},
+        {provider_name: "Looop電気", plan_name: "おうちプラン", price: "#{looop_homePlan}"},
+        {provider_name: "東京ガス", plan_name: "ずっとも電気１", price: "#{tg_zuttomoDenki1}"}
+      ]
+      print "#{output.sort{|a,b| a[:price] <=> b[:price]}}\n"
+    elsif @@acceptableAmperes.include?(amp)
+      $stderr.puts "電気の使用量は整数を入力してください。(例) simulator = Simulator.new(30, 100)"
+    else usage.is_a?(Integer)
+      $stderr.puts "アンペアは10,15,20,30,40,50,60のうちから一つを選んで入力してください (例) simulator = Simulator.new(30, 100)"
+    end
+  end
+
+# 東京電力エネルギーパートナー従量電灯B
+  def ep_planB
+    @@EP_planB_SET[:rate].sort{|a,b| a[0] <=> b[0]}
+    totalCharge = @@EP_planB_SET[:basicChargeRate] * amp + usageBasedCharge(@@EP_planB_SET[:rate].keys, @@EP_planB_SET[:rate].values)
+    return totalCharge.floor
+  end
+# Looopeおうち電気プラン
+  def looop_homePlan
+    usageBasedCharge = (@usage * 26.40)
+    return usageBasedCharge.floor
+  end
+# 東京ガスずっとも電気１
+  def tg_zuttomoDenki1
+    @@TG_ZUTTOMO_SET[:rate].sort{|a,b| a[0] <=> b[0]}
+    new_amp = amp < 30? 30 : amp
+    totalCharge = @@TG_ZUTTOMO_SET[:basicChargeRate] * new_amp + usageBasedCharge(@@TG_ZUTTOMO_SET[:rate].keys, @@TG_ZUTTOMO_SET[:rate].values)
+    return totalCharge.floor
+  end
+
+# 使用量によって変動する料金を各プランの従量制に基づいて計算する
+  def usageBasedCharge keys, values
+    keys.sort
+    values.sort
+    usageBasedCharge = 0
+    i = 0
+    (values.length - 1).times do 
+      if usage >= keys[i + 1]
+        usageBasedCharge += values[i] * (keys[i + 1] - keys[i])
+        i += 1
+      end
+    end
+    usageBasedCharge += values[i] * (@usage - keys[i])
+  end
+
+# 各プランを変更、拡張するクラスメソッド
+  class << self
+    def EP_planB_SET
+      @@EP_planB_SET
+    end
+
+    def TG_ZUTTOMO_SET
+      @@TG_ZUTTOMO_SET
+    end
+
+    def acceptableAmperes
+      @@acceptableAmperes
+    end
+  end
+end
+
+
+
+
+# プランの規格変更の例
+
+# # 1kWhのあたりの料金設定を帰る
+# Simulator.EP_planB_SET[:rate][0] = 20.00
+# Simulator.EP_planB_SET[:rate].delete(120)
+# Simulator.EP_planB_SET[:rate].update({350 => 50.00})
+# # 基本料金が変わった時
+# Simulator.EP_planB_SET[:basicChargeRate] = 10000.00
+# # 申し込みできるアンペア数が変わった時
+# Simulator.acceptableAmpere << 25
+
+simulator = Simulator.new(30, 300)
+# simulator.simulate
+
+
+
+
+
---
+++ challengeA/yoshikawa_keishi/spec/lib/simulator_spec.rb
@@ -0,0 +1,171 @@
+require "spec_helper"
+require "simulator"
+
+describe Simulator do
+  let(:random_amp){[10, 15, 20, 30, 40, 50, 60].at(rand(6))}
+  let(:random_amp2){[30, 40, 50, 60].at(rand(3))}
+  let(:random_amp3){[10, 15, 20].at(rand(2))}
+  let(:random_usage){rand(0..1000)}
+  let(:simulator){Simulator.new(random_amp, random_usage)}
+  EP_planB_SET = {rate: {0 => 19.88, 120 => 26.48, 300 => 30.57}, basicChargeRate: 28.6 }
+  TG_ZUTTOMO_SET = {rate: {0 => 23.67, 140 => 23.88, 350 => 26.41}, basicChargeRate: 28.6 }
+
+
+  describe "self#EP_planB_SET" do
+    subject { Simulator.EP_planB_SET }
+    it { is_expected.to eq EP_planB_SET }
+  end
+
+  describe "self#TG_ZUTTOMO_SET" do
+    subject { Simulator.TG_ZUTTOMO_SET }
+    it { is_expected.to eq TG_ZUTTOMO_SET }
+  end
+
+  describe "self#acceptableAmperes" do
+    subject { Simulator.acceptableAmperes }
+    it { is_expected.to eq [10, 15, 20, 30, 40, 50, 60] }
+  end
+
+  describe '#attr_reader' do
+    it "show the ampere initialized" do
+      expect(simulator.amp).to eq random_amp
+    end
+
+    it "show the usage initialized" do
+      expect(simulator.usage).to eq random_usage
+    end
+  end
+
+  describe 'Loope おうちプラン' do
+    it "calculates charge of Looop's homePlan" do
+      simulator = Simulator.new(random_amp, random_usage)
+      expected_charge =  26.40 * random_usage
+      expect(simulator.looop_homePlan).to eq expected_charge.floor
+    end
+  end
+
+  describe '#ep_planB' do
+    it " 東京電力従量電灯B 10~60A, 第二段階を超えない使用量" do
+      random_usage = rand(0..EP_planB_SET[:rate].keys[1])
+      simulator = Simulator.new(random_amp, random_usage)
+      expected_charge =  EP_planB_SET[:basicChargeRate] * random_amp + EP_planB_SET[:rate].values[0] * random_usage
+      expect(simulator.ep_planB).to eq expected_charge.floor
+    end
+
+    it "東京電力従量電灯B 10~60A, 第三段階を超えない使用量" do
+      random_usage = rand((EP_planB_SET[:rate].keys[1] + 1)..EP_planB_SET[:rate].keys[2])
+      simulator = Simulator.new(random_amp, random_usage)
+      expected_charge =  EP_planB_SET[:basicChargeRate] * random_amp + EP_planB_SET[:rate].values[0] * EP_planB_SET[:rate].keys[1] + EP_planB_SET[:rate].values[1] * (random_usage - EP_planB_SET[:rate].keys[1])
+      expect(simulator.ep_planB).to eq expected_charge.floor
+    end
+
+    it "東京電力従量電灯B 10~60A, 第三段階を超える使用量" do
+      random_usage = rand((EP_planB_SET[:rate].keys[2] + 1)..1000)
+      simulator = Simulator.new(random_amp, random_usage)
+      expected_charge =  EP_planB_SET[:basicChargeRate] * random_amp + EP_planB_SET[:rate].values[0] * EP_planB_SET[:rate].keys[1] + EP_planB_SET[:rate].values[1] * (EP_planB_SET[:rate].keys[2] - EP_planB_SET[:rate].keys[1]) + EP_planB_SET[:rate].values[2] * (random_usage - EP_planB_SET[:rate].keys[2])
+      expect(simulator.ep_planB).to eq expected_charge.floor
+    end
+  end
+
+  describe '#tg_zuttomoDenki1' do
+    it "東京ガスずっとも電気１ 30~60A, 第二段階を超えない使用量" do
+      random_usage = rand(0..TG_ZUTTOMO_SET[:rate].keys[1])
+      simulator = Simulator.new(random_amp2, random_usage)
+      expected_charge =  TG_ZUTTOMO_SET[:basicChargeRate] * random_amp2 + TG_ZUTTOMO_SET[:rate].values[0] * random_usage
+      expect(simulator.tg_zuttomoDenki1).to eq expected_charge.floor
+    end
+
+    it "東京ガスずっとも電気１ 30~60A, 第三段階を超えない使用量" do
+      random_usage = rand((TG_ZUTTOMO_SET[:rate].keys[1] + 1)..TG_ZUTTOMO_SET[:rate].keys[2])
+      simulator = Simulator.new(random_amp2, random_usage)
+      expected_charge =  TG_ZUTTOMO_SET[:basicChargeRate] * random_amp2 + TG_ZUTTOMO_SET[:rate].values[0] * TG_ZUTTOMO_SET[:rate].keys[1] + TG_ZUTTOMO_SET[:rate].values[1] * (random_usage - TG_ZUTTOMO_SET[:rate].keys[1])
+      expect(simulator.tg_zuttomoDenki1).to eq expected_charge.floor
+    end
+
+    it "東京ガスずっとも電気１ 30~60A, 第三段階を超える使用量" do
+      random_usage = rand((TG_ZUTTOMO_SET[:rate].keys[2] + 1)..1000)
+      simulator = Simulator.new(random_amp2, random_usage)
+      expected_charge =  TG_ZUTTOMO_SET[:basicChargeRate] * random_amp2 + TG_ZUTTOMO_SET[:rate].values[0] * TG_ZUTTOMO_SET[:rate].keys[1] + TG_ZUTTOMO_SET[:rate].values[1] * (TG_ZUTTOMO_SET[:rate].keys[2] - TG_ZUTTOMO_SET[:rate].keys[1]) + TG_ZUTTOMO_SET[:rate].values[2] * (random_usage - TG_ZUTTOMO_SET[:rate].keys[2])
+      expect(simulator.tg_zuttomoDenki1).to eq expected_charge.floor
+    end
+
+    it "東京ガスずっとも電気１ 30Aに満たない, 第二段階を超えない使用量" do
+      random_usage = rand(0..TG_ZUTTOMO_SET[:rate].keys[1])
+      simulator = Simulator.new(random_amp3, random_usage)
+      expected_charge =  TG_ZUTTOMO_SET[:basicChargeRate] * 30 + TG_ZUTTOMO_SET[:rate].values[0] * random_usage
+      expect(simulator.tg_zuttomoDenki1).to eq expected_charge.floor
+    end
+
+    it "東京ガスずっとも電気１ 30Aに満たない, 第三段階を超えない使用量" do
+      random_usage = rand((TG_ZUTTOMO_SET[:rate].keys[1] + 1)..TG_ZUTTOMO_SET[:rate].keys[2])
+      simulator = Simulator.new(random_amp3, random_usage)
+      expected_charge =  TG_ZUTTOMO_SET[:basicChargeRate] * 30 + TG_ZUTTOMO_SET[:rate].values[0] * TG_ZUTTOMO_SET[:rate].keys[1] + TG_ZUTTOMO_SET[:rate].values[1] * (random_usage - TG_ZUTTOMO_SET[:rate].keys[1])
+      expect(simulator.tg_zuttomoDenki1).to eq expected_charge.floor
+    end
+
+    it "東京ガスずっとも電気１ 30Aに満たない, 第三段階を超える使用量" do
+      random_usage = rand((TG_ZUTTOMO_SET[:rate].keys[2] + 1)..1000)
+      simulator = Simulator.new(random_amp3, random_usage)
+      expected_charge =  TG_ZUTTOMO_SET[:basicChargeRate] * 30 + TG_ZUTTOMO_SET[:rate].values[0] * TG_ZUTTOMO_SET[:rate].keys[1] + TG_ZUTTOMO_SET[:rate].values[1] * (TG_ZUTTOMO_SET[:rate].keys[2] - TG_ZUTTOMO_SET[:rate].keys[1]) + TG_ZUTTOMO_SET[:rate].values[2] * (random_usage - TG_ZUTTOMO_SET[:rate].keys[2])
+      expect(simulator.tg_zuttomoDenki1).to eq expected_charge.floor
+    end
+  end
+
+  describe "#simulate" do
+    context('引数に誤りがない場合') do 
+
+      context("どのプランの規定にも変更がない場合") do
+        it "puts an array of hashes summaring each plan's data" do
+          simulator = Simulator.new(random_amp, random_usage)
+          output = [
+            {provider_name: "東京電力エナジーパートナー", plan_name: "従量電灯B", price: "#{simulator.ep_planB}"},
+            {provider_name: "Looop電気", plan_name: "おうちプラン", price: "#{simulator.looop_homePlan}"},
+            {provider_name: "東京ガス", plan_name: "ずっとも電気１", price: "#{simulator.tg_zuttomoDenki1}"}
+          ]
+          output = output.sort{|a,b| a[:price] <=> b[:price]}
+          expect{simulator.simulate}.to output("#{output}\n").to_stdout
+        end
+      end
+
+      context("もし東電従量電灯Bの設定に変更があった場合") do
+        it "puts an array of hashes summaring each plan's data(レートを追加した時）" do
+          Simulator.EP_planB_SET[:rate].update({400 => 50.00})
+          simulator = Simulator.new(random_amp, random_usage)
+          output = [
+            {provider_name: "東京電力エナジーパートナー", plan_name: "従量電灯B", price: "#{simulator.ep_planB}"},
+            {provider_name: "Looop電気", plan_name: "おうちプラン", price: "#{simulator.looop_homePlan}"},
+            {provider_name: "東京ガス", plan_name: "ずっとも電気１", price: "#{simulator.tg_zuttomoDenki1}"}
+          ]
+          output = output.sort{|a,b| a[:price] <=> b[:price]}
+          expect{simulator.simulate}.to output("#{output}\n").to_stdout
+        end
+      end
+
+      context("もし東京ガスずっとも電気１の設定に変更があった場合") do
+        it "puts an array of hashes summaring each plan's data（正しい順序でないレートを誤まって設定した時）" do
+          Simulator.TG_ZUTTOMO_SET[:rate].replace({300 => 30.57, 120 => 26.48, 0 => 19.88})
+          simulator = Simulator.new(random_amp, random_usage)
+          output = [
+            {provider_name: "東京電力エナジーパートナー", plan_name: "従量電灯B", price: "#{simulator.ep_planB}"},
+            {provider_name: "Looop電気", plan_name: "おうちプラン", price: "#{simulator.looop_homePlan}"},
+            {provider_name: "東京ガス", plan_name: "ずっとも電気１", price: "#{simulator.tg_zuttomoDenki1}"}
+          ]
+          output = output.sort{|a,b| a[:price] <=> b[:price]}
+          expect{simulator.simulate}.to output("#{output}\n").to_stdout
+        end
+      end
+    end
+
+    context('引数に誤りがある場合') do 
+      it "puts error message if ampere is invalid" do
+        simulator = Simulator.new("a", random_usage)
+        expect{simulator.simulate}.to output("アンペアは10,15,20,30,40,50,60のうちから一つを選んで入力してください (例) simulator = Simulator.new(30, 100)\n").to_stderr
+      end
+
+      it "puts error message if usage is invalid" do
+        simulator = Simulator.new(random_amp, "100")
+        expect{simulator.simulate}.to output("電気の使用量は整数を入力してください。(例) simulator = Simulator.new(30, 100)\n").to_stderr
+      end
+    end
+  end
+end
