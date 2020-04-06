7668a94aa94ab99407a92a204c21bf636b87eb9a
kumackey <kumak1t09e0@gmail.com>
2020-04-06 09:10:50 +0900

    setting of rails g controller

--- challenge3/webapp/kumaki/config/application.rb
+++ challenge3/webapp/kumaki/config/application.rb
@@ -31,5 +31,18 @@ module Kumaki
     config.generators.system_tests = nil
 
     config.generators.template_engine = :slim
+
+    config.generators do |g|
+      g.template_engine = :slim
+      g.assets false
+      g.skip_routes true
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
