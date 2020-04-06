c09e7b30335b8584ec36f1bd83076bd3fde2376a
kumackey <kumak1t09e0@gmail.com>
2020-04-06 20:04:26 +0900

    change routes of datesets lol
    move houses/:house_id/datasets to houses/:id

--- challenge3/webapp/kumaki/app/controllers/houses_controller.rb
+++ challenge3/webapp/kumaki/app/controllers/houses_controller.rb
@@ -2,4 +2,12 @@ class HousesController < ApplicationController
   def index
     @houses = House.includes(:city).page(params[:page]).per(10).order(:id)
   end
+
+  def show
+    @datasets = Dataset.search_with_house_id(params[:id]).order(:id)
+    @data_for_chart = []
+    @datasets.each do |dataset|
+      @data_for_chart << [dataset.date, dataset.energy_production]
+    end
+  end
 end
--- challenge3/webapp/kumaki/config/routes.rb
+++ challenge3/webapp/kumaki/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :cities, only: %i[index]
-  resources :houses, only: %i[index], shallow: true do
-    resources :datasets, only: %i[index]
-  end
+  resources :houses, only: %i[index, show]
+  resources :datasets, only: %i[index]
 end
