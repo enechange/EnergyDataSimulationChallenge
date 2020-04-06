bd00a83915e149c6bd9ad0819aadd81814f0ffed
kumackey <kumak1t09e0@gmail.com>
2020-04-07 08:00:14 +0900

    request spec of GET 'houses/1'

--- challenge3/webapp/kumaki/spec/requests/houses_spec.rb
+++ challenge3/webapp/kumaki/spec/requests/houses_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe 'Houses', type: :request do
     expect(response).to have_http_status(200)
     expect(response.body).to include('Carolyn')
   end
+
+  it 'House詳細画面の表示に成功すること' do
+    dataset = create(:dataset, daylight: 178.9)
+    get "/houses/#{dataset.house.id}"
+    expect(response).to have_http_status(200)
+    expect(response.body).to include('178.9')
+  end
 end
