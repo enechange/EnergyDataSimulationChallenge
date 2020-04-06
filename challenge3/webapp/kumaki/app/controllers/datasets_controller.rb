bd2981bb95963f1dece368fb02acd24ad4e562df
kumackey <kumak1t09e0@gmail.com>
2020-04-07 08:06:55 +0900

    display datasets#index as resource datasets

--- challenge3/webapp/kumaki/app/controllers/datasets_controller.rb
+++ challenge3/webapp/kumaki/app/controllers/datasets_controller.rb
@@ -1,9 +1,5 @@
 class DatasetsController < ApplicationController
   def index
-    @datasets = Dataset.search_with_house_id(params[:house_id]).order(:id)
-    @data_for_chart = []
-    @datasets.each do |dataset|
-      @data_for_chart << [dataset.date, dataset.energy_production]
-    end
+    @datasets = Dataset.page(params[:page]).per(100).order(:id)
   end
 end
