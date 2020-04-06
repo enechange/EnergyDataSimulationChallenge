a61762821b8e4145ddc76539b9ee2d289d934a11
kumackey <kumak1t09e0@gmail.com>
2020-04-06 09:23:59 +0900

    change TimeZone

--- challenge3/webapp/kumaki/config/application.rb
+++ challenge3/webapp/kumaki/config/application.rb
@@ -44,5 +44,8 @@ module Kumaki
                        controller_specs: false
       g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
