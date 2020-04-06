a8a3a1fc35f9370bd7168fe1b64a53df1a5e65a7
kumackey <kumak1t09e0@gmail.com>
2020-04-06 19:55:19 +0900

    Time series analysis of EnergyProduction by house

--- challenge3/webapp/kumaki/app/controllers/datasets_controller.rb
+++ challenge3/webapp/kumaki/app/controllers/datasets_controller.rb
@@ -1,5 +1,9 @@
 class DatasetsController < ApplicationController
   def index
     @datasets = Dataset.search_with_house_id(params[:house_id]).order(:id)
+    @data_for_chart = []
+    @datasets.each do |dataset|
+      @data_for_chart << [dataset.date, dataset.energy_production]
+    end
   end
 end
