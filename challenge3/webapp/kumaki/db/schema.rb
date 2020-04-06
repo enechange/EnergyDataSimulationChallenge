cf8708e4c5df84087fd2588e0e65ef14cb1a1ed4
kumackey <kumak1t09e0@gmail.com>
2020-04-06 12:52:18 +0900

    create cities table and model

--- challenge3/webapp/kumaki/db/schema.rb
+++ challenge3/webapp/kumaki/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_004522) do
+ActiveRecord::Schema.define(version: 2020_04_06_034852) do
+
+  create_table "cities", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "houses", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "firstname", null: false
