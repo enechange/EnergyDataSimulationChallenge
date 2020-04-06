a09ac605538a079cca4614365dc38d31220405d1
kumackey <kumak1t09e0@gmail.com>
2020-04-06 21:18:10 +0900

    cities factory and check displaying the name

--- challenge3/webapp/kumaki/spec/requests/cities_spec.rb
+++ challenge3/webapp/kumaki/spec/requests/cities_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Cities', type: :request do
   it 'Cities一覧画面の表示に成功すること' do
+    create(:city, name: 'London')
     get '/cities'
     expect(response).to have_http_status(200)
+    expect(response.body).to include('London')
   end
 end
