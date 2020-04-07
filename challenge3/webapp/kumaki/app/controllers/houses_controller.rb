9c9c34d52296bac229530fbf00fad6af336a0f3a
kumackey <kumak1t09e0@gmail.com>
2020-04-07 23:15:11 +0900

    create column chart and table of num_of_people

--- challenge3/webapp/kumaki/app/controllers/houses_controller.rb
+++ challenge3/webapp/kumaki/app/controllers/houses_controller.rb
@@ -3,6 +3,9 @@ class HousesController < ApplicationController
     @count_city = House.joins(:city).group(:city_id)
                     .select('MAX(city_id) AS city_id, cities.name AS city_name, COUNT(*) AS count_all')
                     .order(count_all: :desc).map{ |house| [house.city_name, house.count_all] }.to_h
+    @num_of_people = House.group(:num_of_people)
+                          .select('MAX(num_of_people) AS num_of_people, COUNT(*) AS count_all')
+                          .order(:num_of_people).map{ |house| [house.num_of_people, house.count_all] }.to_h
     @houses_for_pagination = House.includes(:city).page(params[:page]).per(10).order(:id)
   end
 
