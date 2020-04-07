870808326cabadf771f992952a2855b672a78a54
kumackey <kumak1t09e0@gmail.com>
2020-04-07 09:44:23 +0900

    add scatter plot

--- challenge3/webapp/kumaki/app/helpers/application_helper.rb
+++ challenge3/webapp/kumaki/app/helpers/application_helper.rb
@@ -22,4 +22,21 @@ module ApplicationHelper
     end
     data
   end
+
+  def data_of_daylight_with_temperature(datasets)
+    data = []
+    datasets.each do |dataset|
+      data << [dataset.temperature, dataset.daylight]
+    end
+    data
+  end
+
+  def data_of_energy_production_with_daylight(datasets)
+    data = []
+    datasets.each do |dataset|
+      data << [dataset.daylight, dataset.energy_production]
+    end
+    data
+  end
+
 end
