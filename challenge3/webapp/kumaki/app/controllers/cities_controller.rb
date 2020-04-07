1ee4f95b0b0391143b4a57f49978c1cb40377958
kumackey <kumak1t09e0@gmail.com>
2020-04-07 18:08:31 +0900

    line chart of average datasets

--- challenge3/webapp/kumaki/app/controllers/cities_controller.rb
+++ challenge3/webapp/kumaki/app/controllers/cities_controller.rb
@@ -5,5 +5,6 @@ class CitiesController < ApplicationController
 
   def show
     @cities = City.where(id: params[:id])
+    @datasets = Dataset.joins(house: :city).where(cities: {id: params[:id]}).select('datasets.year_month, AVG(datasets.temperature) AS temperature, AVG(datasets.daylight) AS daylight, AVG(datasets.energy_production) AS energy_production').group(:year_month).order(:year_month)
   end
 end
