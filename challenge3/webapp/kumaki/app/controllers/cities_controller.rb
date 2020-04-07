aeeaa371ebfcaa2c7b74c52795a910b80a95c3f7
kumackey <kumak1t09e0@gmail.com>
2020-04-07 19:04:31 +0900

    devide datasets into for charts and for pagination

--- challenge3/webapp/kumaki/app/controllers/cities_controller.rb
+++ challenge3/webapp/kumaki/app/controllers/cities_controller.rb
@@ -8,6 +8,7 @@ class CitiesController < ApplicationController
     @datasets_group_by_time = Dataset.search_with_city_id(params[:id])
                                      .select_average_datasets_group_by(:year_month)
     @average_dataset = Dataset.search_with_city_id(params[:id]).select_average_dataset
-    @datasets = Dataset.search_with_city_id(params[:id]).includes(:house).page(params[:page]).per(50)
+    @datasets = Dataset.search_with_city_id(params[:id]).includes(:house)
+    @datasets_for_pagination = @datasets.page(params[:page]).per(10)
   end
 end
