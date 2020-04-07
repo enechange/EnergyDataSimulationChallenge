9a05b316054d8310bad012f020838279f4e18681
kumackey <kumak1t09e0@gmail.com>
2020-04-07 14:27:41 +0900

    add average table to datasets#index

--- challenge3/webapp/kumaki/app/controllers/datasets_controller.rb
+++ challenge3/webapp/kumaki/app/controllers/datasets_controller.rb
@@ -1,6 +1,7 @@
 class DatasetsController < ApplicationController
   def index
     @datasets_group_by_time = Dataset.select('datasets.year_month, AVG(datasets.temperature) AS temperature, AVG(datasets.daylight) AS daylight, AVG(datasets.energy_production) AS energy_production').group(:year_month).order(:year_month)
+    @average_dataset = Dataset.select_average
     @datasets = Dataset.order(:id)
     @datasets_for_pagination = @datasets.includes(:house).page(params[:page]).per(50)
   end
