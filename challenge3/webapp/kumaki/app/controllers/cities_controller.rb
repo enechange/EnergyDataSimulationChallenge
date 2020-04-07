b08684d664382ee229224306393278bc56b4d6ff
kumackey <kumak1t09e0@gmail.com>
2020-04-07 18:37:21 +0900

    add scatter charts and datasets

--- challenge3/webapp/kumaki/app/controllers/cities_controller.rb
+++ challenge3/webapp/kumaki/app/controllers/cities_controller.rb
@@ -5,8 +5,9 @@ class CitiesController < ApplicationController
 
   def show
     @cities = City.where(id: params[:id])
-    @datasets = Dataset.search_with_city_id(params[:id])
-                      .select_average_datasets_group_by(:year_month)
+    @datasets_group_by_time = Dataset.search_with_city_id(params[:id])
+                                     .select_average_datasets_group_by(:year_month)
     @average_dataset = Dataset.search_with_city_id(params[:id]).select_average_dataset
+    @datasets = Dataset.search_with_city_id(params[:id]).includes(:house).page(params[:page]).per(50)
   end
 end
