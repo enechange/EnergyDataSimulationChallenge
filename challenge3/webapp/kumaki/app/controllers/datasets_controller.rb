b5721db7aca3d02bfc3abfcea90daa66f5a4d9a3
kumackey <kumak1t09e0@gmail.com>
2020-04-07 11:48:09 +0900

    average chart in datasets page

--- challenge3/webapp/kumaki/app/controllers/datasets_controller.rb
+++ challenge3/webapp/kumaki/app/controllers/datasets_controller.rb
@@ -1,5 +1,7 @@
 class DatasetsController < ApplicationController
   def index
-    @datasets = Dataset.includes(:house).page(params[:page]).per(100).order(:id)
+    @datasets_group_by_time = Dataset.select('datasets.year_month, AVG(datasets.temperature) AS temperature, AVG(datasets.daylight) AS daylight, AVG(datasets.energy_production) AS energy_production').group(:year_month).order(:year_month)
+    @datasets = Dataset.order(:id)
+    @datasets_for_pagination = @datasets.includes(:house).page(params[:page]).per(50)
   end
 end
