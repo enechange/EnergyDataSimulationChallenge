c81da3423bfcd4f765392479f62701141b2c544a
kumackey <kumak1t09e0@gmail.com>
2020-04-07 22:15:33 +0900

    use map method at application helper

--- challenge3/webapp/kumaki/app/helpers/application_helper.rb
+++ challenge3/webapp/kumaki/app/helpers/application_helper.rb
@@ -1,42 +1,22 @@
 module ApplicationHelper
   def data_of_temperature_with_time(datasets)
-    data = []
-    datasets.each do |dataset|
-      data << [dataset.year_month, dataset.temperature]
-    end
-    data
+    datasets.map { |dataset| [dataset.year_month, dataset.temperature] }
   end
 
   def data_of_daylight_with_time(datasets)
-    data = []
-    datasets.each do |dataset|
-      data << [dataset.year_month, dataset.daylight]
-    end
-    data
+    datasets.map { |dataset| [dataset.year_month, dataset.daylight] }
   end
 
   def data_of_energy_production_with_time(datasets)
-    data = []
-    datasets.each do |dataset|
-      data << [dataset.year_month, dataset.energy_production]
-    end
-    data
+    datasets.map { |dataset| [dataset.year_month, dataset.energy_production] }
   end
 
   def data_of_daylight_with_temperature(datasets)
-    data = []
-    datasets.each do |dataset|
-      data << [dataset.temperature, dataset.daylight]
-    end
-    data
+    datasets.map { |dataset| [dataset.temperature, dataset.daylight] }
   end
 
   def data_of_energy_production_with_daylight(datasets)
-    data = []
-    datasets.each do |dataset|
-      data << [dataset.daylight, dataset.energy_production]
-    end
-    data
+    datasets.map { |dataset| [dataset.daylight, dataset.energy_production] }
   end
 
   def convert_into_yes_or_no(boolean)
