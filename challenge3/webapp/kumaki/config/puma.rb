b6da6c6289f6961a830767381f83fa212623220f
kumackey <kumak1t09e0@gmail.com>
2020-04-08 12:34:38 +0900

    fix after deploy

--- challenge3/webapp/kumaki/config/puma.rb
+++ challenge3/webapp/kumaki/config/puma.rb
@@ -8,7 +8,7 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 port        ENV.fetch('PORT') { 3000 }
 
 # production
-# tmp_path = '/var/www/book-output/tmp'
+# tmp_path = '/var/www/EnergyDataSimulationChallenge/challenge3/webapp/kumaki/tmp'
 # bind "unix://#{tmp_path}/sockets/puma.sock"
 # workers 2
 # preload_app!
