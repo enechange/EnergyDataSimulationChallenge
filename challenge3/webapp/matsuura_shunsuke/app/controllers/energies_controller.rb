76012ea648dbbbe0708ce8e00a8440d206752b56
shumatsuura <shunsuke.mtr@gmail.com>
2020-05-31 13:27:08 +0900

    create seed.rb

--- challenge3/webapp/matsuura_shunsuke/app/controllers/energies_controller.rb
+++ challenge3/webapp/matsuura_shunsuke/app/controllers/energies_controller.rb
@@ -1,4 +1,5 @@
 class EnergiesController < ApplicationController
   def index
+    
   end
 end
--- challenge3/webapp/matsuura_shunsuke/db/seeds.rb
+++ challenge3/webapp/matsuura_shunsuke/db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach('../../data/house_data.csv', headers: true) do |row|
+  House.create(
+    first_name: row['Firstname'],
+    last_name: row['Lastname'],
+    city: row['City'],
+    num_of_people: row['num_of_people'],
+    has_child: row['has_child'] == 'Yes' ? true : false,
+                )
+end
+
+CSV.foreach('../../data/dataset_50.csv', headers: true) do |row|
+  Energy.create(
+    label: row['Label'],
+    house_id: row['House'],
+    temperature: row['Temperature'],
+    daylight: row['Daylight'],
+    energy_production: row['EnergyProduction'],
+    year_month: row['Year'] + '/' + row['Month'],
+                )
+end
