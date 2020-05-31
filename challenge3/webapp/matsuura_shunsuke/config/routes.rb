dcc9be6fdb70bfae0aa72f75d460b65bb239487a
shumatsuura <shunsuke.mtr@gmail.com>
2020-05-31 13:02:40 +0900

    create energies controller and view

--- challenge3/webapp/matsuura_shunsuke/config/routes.rb
+++ challenge3/webapp/matsuura_shunsuke/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'energies/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'energies#index'
   resources :energies
