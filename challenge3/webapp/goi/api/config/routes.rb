c0a67ca904dd53afbadd3d38b2db4711d37f3f51
goi <kakedashiit@gmail.com>
2020-07-11 00:13:09 +0900

    add: create base routing

--- challenge3/webapp/goi/api/config/routes.rb
+++ challenge3/webapp/goi/api/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      resources :house, only: %i[index]
+      resources :energy, only: %i[index]
+    end
+
+    get '*path', to: 'home#index'
+  end
 end
