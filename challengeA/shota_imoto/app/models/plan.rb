c813d54cb41abc8d347e7a43b5ecb6eddc24b752
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-10-09 16:01:15 +0900

    fix_calcurate_error
    ## what
    -計算結果がマイナスになるエラーを修正。(従量料金計算時に、例）100kWhのときに120~300kWhの電力量について計算していた→(100-123)*従量料金)
    
    ## why

--- challengeA/shota_imoto/app/models/plan.rb
+++ challengeA/shota_imoto/app/models/plan.rb
@@ -6,6 +6,7 @@ class Plan < ApplicationRecord
   def calculate_usage_charge(power_consumption)
     sum_usage_charge = 0
     self.usage_charges.each do |usage_charge|
+      break if power_consumption < usage_charge.lower_power
       sum_usage_charge += usage_charge.calculate_usage_charge_each_of_range(power_consumption)
     end
     return sum_usage_charge
