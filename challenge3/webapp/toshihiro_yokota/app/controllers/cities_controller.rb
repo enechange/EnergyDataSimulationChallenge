13301ba8096a8b6e56f27b63934f16bf7851a901
yokoto <yo44ko55@gmail.com>
2020-10-10 15:42:36 +0900

    modify City.eager_load(houses: :energies) to City.eager_load(:energies)

--- challenge3/webapp/toshihiro_yokota/app/controllers/cities_controller.rb
+++ challenge3/webapp/toshihiro_yokota/app/controllers/cities_controller.rb
@@ -1,6 +1,6 @@
 class CitiesController < ApplicationController
   def index
-    cities = City.eager_load(houses: :energies)
+    cities = City.eager_load(:energies)
     @cities_data = CityChartService.call(cities)
     @dates = Energy.dates
   end
