d7ae7315a4312a602b0750d683a8afd94920786a
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2020-10-11 16:27:49 +0900

    fix_Floating_point_error
    ## what
    - 浮動小数点誤差を修正

--- challengeA/shota_imoto/app/models/usage_charge.rb
+++ challengeA/shota_imoto/app/models/usage_charge.rb
@@ -1,10 +1,17 @@
 class UsageCharge < ApplicationRecord
+  require 'bigdecimal'
+
   belongs_to :plan
   def calculate_usage_charge_each_of_range(power_consumption)
+    binding.pry
     if power_consumption > self.upper_power
-      self.charge * (self.upper_power - self.lower_power)
+      BigDecimal(self.charge_s) * (self.upper_power - self.lower_power)
     else
-      self.charge * (power_consumption - self.lower_power)
+      BigDecimal(self.charge_s) * (power_consumption - self.lower_power)
     end
+  end
+
+  def charge_s
+    self.charge.to_s
   end
 end
