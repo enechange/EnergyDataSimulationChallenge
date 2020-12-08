e37421abc00b2ce8497baae89c90cd3d585a0c36
magatamaamagata <magatamaamagata@gmail.com>
2020-12-08 19:34:37 +0900

    Make simulator class

--- challengeA/Amagata/Simulator.rb
+++ challengeA/Amagata/Simulator.rb
@@ -70,53 +70,61 @@
 
 
 
-# class Simulator
-#   def simulate
-#     result = {}
-#     { provider_name: "#{}", plan_name: "#{}", price: "#{}" }
-#     p result
-#   end
-# end
 
-# class calculator
-#   def initialize(ampare,kilowatt_hour)
-#     @ampare=ampare
-#     @kilowatt_hour=kilowatt_hour
-#   end
-#   def ampare
-#     @ampare
-#   end
-#   def kilowatt_hour
-#     @kilowatt_hour
-#   end
-#     # kWhの条件と計算結果
-#     [tepco=> { "range1" => [@kilowatt_hour < 120,19.88], "range2" => [120,},loooop=>{ "range1" => "", "range2" => ""},tokyo_gas=>{ "range1" => 140, "range2" => 350}]
-#     def calculate
-#       if @kilowatt_hour < 120
-#         sum_tepco = tepco["#{@ampare}A"].to_i + 19.88*@kilowatt_hour
-#       elsif @kilowatt_hour >=120 && @kilowatt_hour < 300
-#         sum_tepco = tepco["#{@ampare}A"].to_i + 26.48*@kilowatt_hour
-#       else
-#         sum_tepco = tepco["#{@ampare}A"].to_i + 30.57*@kilowatt_hour
-#     end  
-#     @sum_tepco= sum_tepco
-#     @array << @sum_tepco
-#     end
-#     lowest = @array.min
-#     result[price]=lowest
-# end
+
+class Calculator
+  def initialize(ampare,kilowatt_hour)
+    @ampare=ampare
+    @kilowatt_hour=kilowatt_hour
+  end
+    # kWhの条件と計算結果
+    # array_all = [tepco=> { "range1" => [@kilowatt_hour < 120,19.88], "range2" => [120,},loooop=>{ "range1" => "", "range2" => ""},tokyo_gas=>{ "range1" => 140, "range2" => 350}]
+
+    def calculate
+      info=Information.new
+      arrays_all=[info.tepco,info.loooop,info.tokyo_gas]
+      results=[]
+    arrays_all.each do |array|
+      i = 0
+      if array[2][i]<= @kilowatt_hour && @kilowatt_hour < array[2][i+1]
+        sum = info.basic_charge["#{@ampare}A"] + array[3][i]*@kilowatt_hour
+      elsif @kilowatt_hour >=array[2][i+1] && @kilowatt_hour < array[2][i+2]
+        sum = info.basic_charge["#{@ampare}A"] + array[3][i+1]*@kilowatt_hour
+      else
+        sum = info.basic_charge["#{@ampare}A"] + array[3][i+2]*@kilowatt_hour
+      end  
+      i += 1
+      results << sum
+    end
+    @lowest = results.min
+    order = results.index(@lowest)
+    @campany = arrays_all[order][0]
+    @plan= arrays_all[order][1]
+  end
+  
+  def lowest
+    @lowest
+  end
+  def campany
+    @campany
+  end
+  def plan
+    @plan
+  end
+end
 
 class Information
   #東京の基本料金
-  @@basic_charge_tokyo= { "10A" => "286", "15A" => "429", "20A" => "572","30A" => "858", "40A" => "1144", "50A" => "1430", "60A" => "1716" }
-  
+  def basic_charge
+    @basic_charge_tokyo= { "10A" => 286, "15A" => 429, "20A" => 572,"30A" => 858, "40A" => 1144, "50A" => 1430, "60A" => 1716 }
+  end
   # 以下、[会社名、プラン名、レンジ、従量料金]
   def tepco
     @tepco = ["TEPCO","従量電灯B",[0,120,300],[19.88,26.48,30.57]]
   end
   
   def  loooop
-    @loooop = ["Loooop","おうちプラン",[0],[26.40]]
+    @loooop = ["Loooop","おうちプラン",[0,0,0],[26.40,26.40,26.40]]
   end
 
   def  tokyo_gas
@@ -124,26 +132,42 @@ class Information
   end
 end
 
-class UserInput
+# class UserInput
+#   # 区分を入力
+#   def ampare
+#      p "契約アンペアを入力してください(A)"
+#     gets.to_i
+#   end
+#   # 使用電力を入力
+#   def kilowatt_hour
+#     p "1ヶ月の電力使用料を入力してください(kWh)"
+#     gets.to_i
+#   end
+# end
+
+class Simulator
+  def simulate
+# ユーザー情報入力
   # 区分を入力
-  def ampare
-    p "契約アンペアを入力してください(A)"
-    gets.to_i
-  end
-  # 使用電力を入力
-  def kilowatt_hour
-    p "1ヶ月の電力使用料を入力してください(kWh)"
-    gets.to_i
+  p "契約アンペアを入力してください(A)"
+  ampare = gets.to_i
+# 使用電力を入力
+  p "1ヶ月の電力使用料を入力してください(kWh)"
+  kilowatt_hour= gets.to_i
+# p @campany
+calc=Calculator.new(ampare,kilowatt_hour)
+calc.calculate
+    result = { provider_name: "#{calc.campany}", plan_name: "#{calc.plan}", price: "#{calc.lowest}" }
+    p result
   end
 end
 # simulator=Simulator.new
 
-data=Information.new
-data.tepco
 
-user_input=UserInput.new
-# ユーザー情報入力
-user_input.ampare
-user_input.kilowatt_hour
-# simulator = Simulator.new(ampere,kilowatt_hour)
-# simulator.simulate
+
+
+
+
+
+simulator = Simulator.new
+simulator.simulate
