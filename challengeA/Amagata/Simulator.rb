80882278af9aae229c502569b16753bc2dc6239d
magatamaamagata <magatamaamagata@gmail.com>
2020-12-09 12:51:07 +0900

    Update basic charge

--- challengeA/Amagata/Simulator.rb
+++ challengeA/Amagata/Simulator.rb
@@ -13,17 +13,26 @@ class Calculator
     price=[]
     # 条件から各社料金を計算
     arrays_all.each do |array|
-      # 基本料金を定義
-      basic = info.basic_charge["#{@ampare}A"]
-      # 単価決定の条件式1
-      if array[:range][0]<= @kilowatt_hour && @kilowatt_hour < array[:range][1]
-        sum = basic + array[:pay_per_use][0] * @kilowatt_hour
-      # 単価決定の条件式2
-      elsif @kilowatt_hour >= array[:range][1] && @kilowatt_hour < array[:range][2]
-        sum = basic + array[:pay_per_use][1] * @kilowatt_hour
-      # 単価決定の条件式3
+      if array[:contract_current].include?(@ampare)
+        # 基本料金を定義 アンペアがbasic_chargeにあればその基本料金料金、なければ0
+        if info.basic_charge[:"#{@ampare}A"]
+          basic = info.basic_charge[:"#{@ampare}A"]
+        else
+          basic = 0
+        end
+        # 単価決定の条件式1 
+        if array[:range][0]<= @kilowatt_hour && @kilowatt_hour < array[:range][1]
+          # 料金=基本料金+従量課金
+          sum = basic + array[:pay_per_use][0] * @kilowatt_hour
+        # 単価決定の条件式2
+        elsif @kilowatt_hour >= array[:range][1] && @kilowatt_hour < array[:range][2]
+          sum = basic + array[:pay_per_use][1] * @kilowatt_hour
+        # 単価決定の条件式3
+        else
+          sum = basic + array[:pay_per_use][2] * @kilowatt_hour
+        end
       else
-        sum = basic + array[:pay_per_use][2] * @kilowatt_hour
+        sum = 0
       end
       price << sum
     end
@@ -31,7 +40,9 @@ class Calculator
     # 料金の計算結果の個数だけ会社とプラント料金を、配列@resultsに入れる。
     @results=[]
     price.length.times do |i|
-      @results << { provider_name: "#{arrays_all[i][:provider_name]}", plan_name: "#{arrays_all[i][:plan_name]}", price: "#{price[i]}" }
+      if price[i] != 0
+        @results << { provider_name: "#{arrays_all[i][:provider_name]}", plan_name: "#{arrays_all[i][:plan_name]}", price: "#{price[i]}" }
+      end
     end
   end
   # 結果ゲッター
@@ -45,16 +56,25 @@ class Information
   #東京の基本料金
   def basic_charge
     @basic_charge_tokyo= { 
-      "10A" => 286, "15A" => 429, "20A" => 572,"30A" => 858, "40A" => 1144, "50A" => 1430, "60A" => 1716 
+      "10A": 286,
+      "15A": 429,
+      "20A": 572,
+      "30A": 858,
+      "40A": 1144,
+      "50A": 1430,
+      "60A": 1716 
     }
   end
+
+
   # 以下、[会社名、プラン名、レンジ、従量料金]
   def tepco
     @tepco = {
       "provider_name": "TEPCO",
       "plan_name": "従量電灯B",
       "range": [0,120,300],
-      "pay_per_use": [19.88,26.48,30.57]
+      "pay_per_use": [19.88,26.48,30.57],
+      "contract_current": [30,40,50,60]
     }
   end
 
@@ -63,7 +83,8 @@ class Information
       "provider_name": "Loooop",
       "plan_name": "おうちプラン",
       "range": [0,0,0],
-      "pay_per_use": [26.40,26.40,26.40]
+      "pay_per_use": [26.40,26.40,26.40],
+      "contract_current": [*10..60]
     }
   end
 
@@ -72,7 +93,8 @@ class Information
       "provider_name": "Tokyo gas",
       "plan_name": "ずっとも電気１",
       "range": [0,140,350],
-      "pay_per_use": [23.67,23.88,26.41]
+      "pay_per_use": [23.67,23.88,26.41],
+      "contract_current": [10,15,20,30,40,50,60]
     }
   end
 #追加用
@@ -82,6 +104,7 @@ class Information
   #     "plan_name": "amagataplan",
   #     "range": [0,200,500],
   #     "pay_per_use": [10,20,30]
+  #     "contract_current": ["10A","50A","60A"]
   #   }
   # end
 end
