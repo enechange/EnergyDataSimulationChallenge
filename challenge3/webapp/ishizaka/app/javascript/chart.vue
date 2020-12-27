97d8b7323311a8e8858ed891ba77563b8f1fa016
ren <yk.r.x.821@gmail.com>
2020-12-27 16:38:51 +0900

    仮のグラフを表示

--- challenge3/webapp/ishizaka/app/javascript/chart.vue
+++ challenge3/webapp/ishizaka/app/javascript/chart.vue
@@ -1,22 +1,47 @@
 <template>
-  <div id="chart">
-    <p>{{ message }}</p>
+  <div class="graph-wrap">
+    <h3 class="title">家庭人数ごとの平均年間エネルギー生産</h3>
+    <BarChart/>
   </div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "チャートを表示します。"
-    }
+import { Bar } from 'vue-chartjs'
+
+var BarChart = {
+  extends: Bar,
+  mounted () {
+    this.renderChart({
+      //ラベル
+      labels:['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      //データ詳細
+      datasets: [{
+        //データタイトル
+        label:'House Energy Production',
+        //データ
+        data: [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
+      }]
+    });
   }
 }
+
+export default {
+  name: 'Chart',
+  components: {
+    BarChart
+  }
+} 
 </script>
 
 <style scoped>
-p {
-  font-size: 18px;
+.graph-wrap {
+  width: 85%;
+  height: 40%;
+  margin: 0 auto;
+}
+
+.graph-wrap > .title {
+  font-size: 24px;
   text-align: center;
   color: red;
 }
