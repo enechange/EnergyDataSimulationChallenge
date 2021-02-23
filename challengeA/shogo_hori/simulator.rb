3ea181eced6287f9a0ec05f1fd5bd1301e31678b
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-02-24 01:25:07 +0900

    csvフォーマット作成

--- challengeA/shogo_hori/simulator.rb
+++ challengeA/shogo_hori/simulator.rb
@@ -14,6 +14,7 @@ class Simulator
     plans << { provider_name: '東京電力エナジーパートナー', plan_name: '従量電灯B', price: "#{charge.tepco}" }
     plans << { provider_name: 'Looopでんき', plan_name: 'おうちでんきプラン', price: "#{charge.looop}" }
     plans << { provider_name: '東京ガス', plan_name: 'ずっとも電気１', price: "#{charge.tokyo_gas}" } if [30, 40, 50, 60].include?(@amps)
+    # plans << { provider_name: 【電力会社】, plan_name: 【プラン】, price: 【電気料金】 }
 
     plans.each do |plan|
       puts plan
