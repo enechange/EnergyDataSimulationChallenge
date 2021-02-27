43761b6aa9cfa299f1efe214d397709ad0079c66
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-02-27 09:43:33 +0900

    simulatorテスト作成

--- challengeA/shogo_hori/lib/simulator.rb
+++ challengeA/shogo_hori/lib/simulator.rb
@@ -5,6 +5,11 @@ class Simulator
   attr_reader :amps, :usage
 
   def initialize(amps, usage)
+    @amps = amps
+    @usage = usage
+  end
+
+  def simulate
     if !amps?(amps) && !number?(usage)
       puts "アンペアは10, 15, 20, 30, 40, 50, 60中から入力ください。\n使用量は0以上の数値で入力ください。"
     elsif !number?(usage)
@@ -12,28 +17,23 @@ class Simulator
     elsif !amps?(amps)
       puts 'アンペアは10, 15, 20, 30, 40, 50, 60中から入力ください。'
     else
-      @amps = amps
-      @usage = usage
-    end
-  end
+      charge = Charges.new(@amps, @usage)
+      plans = []
+      plans << { provider_name: Plan::TEPCO[:provider_name], plan_name: Plan::TEPCO[:plan_name],
+                 price: charge.tepco.to_s }
+      plans << { provider_name: Plan::LOOOP[:provider_name], plan_name: Plan::LOOOP[:plan_name],
+                 price: charge.looop.to_s }
+      if [
+        30, 40, 50, 60
+      ].include?(@amps)
+        plans << { provider_name: Plan::TOKYOGAS[:provider_name], plan_name: Plan::TOKYOGAS[:plan_name],
+                   price: charge.tokyogas.to_s }
+      end
+      # plans << { provider_name: 【電力会社】, plan_name: 【プラン】, price: 【電気料金】 }
 
-  def simulate
-    charge = Charges.new(@amps, @usage)
-    plans = []
-    plans << { provider_name: Plan::TEPCO[:provider_name], plan_name: Plan::TEPCO[:plan_name],
-               price: charge.tepco.to_s }
-    plans << { provider_name: Plan::LOOOP[:provider_name], plan_name: Plan::LOOOP[:plan_name],
-               price: charge.looop.to_s }
-    if [
-      30, 40, 50, 60
-    ].include?(@amps)
-      plans << { provider_name: Plan::TOKYOGAS[:provider_name], plan_name: Plan::TOKYOGAS[:plan_name],
-                 price: charge.tokyogas.to_s }
-    end
-    # plans << { provider_name: 【電力会社】, plan_name: 【プラン】, price: 【電気料金】 }
-
-    plans.each do |plan|
-      puts plan
+      plans.each do |plan|
+        puts plan
+      end
     end
   end
 
--- challengeA/shogo_hori/spec/simulate_spec.rb
+++ challengeA/shogo_hori/spec/simulate_spec.rb
@@ -4,24 +4,64 @@ RSpec.describe Simulator do
       it '10A、100kWh' do
         simulator = Simulator.new(10, 100)
         expect(simulator.simulate).to match_array(
-          [{ :provider_name=>'東京電力エナジーパートナー', :plan_name=>'従量電灯B', :price=>'2274' },
-           {:provider_name=>'Looopでんき', :plan_name=>'おうちでんきプラン', :price=>'2640' }]
+          [{ provider_name: '東京電力エナジーパートナー', plan_name: '従量電灯B', price: '2274' },
+           { provider_name: 'Looopでんき', plan_name: 'おうちでんきプラン', price: '2640' }]
         )
       end
 
       it '30A、200kWh' do
         simulator = Simulator.new(30, 200)
         expect(simulator.simulate).to match_array(
-          [{ :provider_name=>'東京電力エナジーパートナー', :plan_name=>'従量電灯B', :price=>'6154' },
-           {:provider_name=>'Looopでんき', :plan_name=>'おうちでんきプラン', :price=>'5280' },
-           {:provider_name=>'東京ガス', :plan_name=>'ずっとも電気１', :price=>'5634' }]
+          [{ provider_name: '東京電力エナジーパートナー', plan_name: '従量電灯B', price: '6154' },
+           { provider_name: 'Looopでんき', plan_name: 'おうちでんきプラン', price: '5280' },
+           { provider_name: '東京ガス', plan_name: 'ずっとも電気１', price: '5634' }]
         )
       end
     end
 
     context '無効なシュミレーション' do
-      it 'A、kWh共に英字' do
-        expect(Simulator.new('a', 'b')).to include "アンペアは10, 15, 20, 30, 40, 50, 60中から入力ください。\n使用量は0以上の数値で入力ください。"
+      it 'アンペア、kWh共に英字' do
+        simulator = Simulator.new('a', 'a')
+        expect do
+          simulator.simulate
+        end.to output("アンペアは10, 15, 20, 30, 40, 50, 60中から入力ください。\n使用量は0以上の数値で入力ください。\n").to_stdout
+      end
+
+      it 'アンペアが英字' do
+        simulator = Simulator.new('a', 100)
+        expect { simulator.simulate }.to output("アンペアは10, 15, 20, 30, 40, 50, 60中から入力ください。\n").to_stdout
+      end
+
+      it 'kWhが英字' do
+        simulator = Simulator.new(10, 'a')
+        expect { simulator.simulate }.to output("使用量は0以上の数値で入力ください。\n").to_stdout
+      end
+
+      it 'アンペア、kWh共にスペース' do
+        simulator = Simulator.new('', '')
+        expect do
+          simulator.simulate
+        end.to output("アンペアは10, 15, 20, 30, 40, 50, 60中から入力ください。\n使用量は0以上の数値で入力ください。\n").to_stdout
+      end
+
+      it 'アンペアがスペース' do
+        simulator = Simulator.new('', 100)
+        expect { simulator.simulate }.to output("アンペアは10, 15, 20, 30, 40, 50, 60中から入力ください。\n").to_stdout
+      end
+
+      it 'kWhがスペース' do
+        simulator = Simulator.new(10, '')
+        expect { simulator.simulate }.to output("使用量は0以上の数値で入力ください。\n").to_stdout
+      end
+
+      it 'アンペアが10, 15, 20, 30, 40, 50, 60以外' do
+        simulator = Simulator.new(59, 100)
+        expect { simulator.simulate }.to output("アンペアは10, 15, 20, 30, 40, 50, 60中から入力ください。\n").to_stdout
+      end
+
+      it 'kWhが負の整数' do
+        simulator = Simulator.new(10, -1)
+        expect { simulator.simulate }.to output("使用量は0以上の数値で入力ください。\n").to_stdout
       end
     end
   end
