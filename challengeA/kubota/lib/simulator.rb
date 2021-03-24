941ba8451e716fd1bae5bea6dc093dab6e0290a7
k-kbot <polykkpodh1243@gmail.com>
2021-03-24 23:07:54 +0900

    " => '

--- challengeA/kubota/lib/simulator.rb
+++ challengeA/kubota/lib/simulator.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require "yaml"
-require "active_model"
+require 'yaml'
+require 'active_model'
 
 class Simulator
   include ActiveModel::Model
 
-  PLANS = YAML.load_file("./plans.yaml")
+  PLANS = YAML.load_file('./plans.yaml')
   CONTRACT_AMPS = [10, 15, 20, 30, 40, 50, 60]
 
   attr_accessor :contract_ampere, :electric_energy_per_month
@@ -20,12 +20,12 @@ class Simulator
   end
 
   def simulate
-    return "無効な契約アンペアまたは電気使用量です。" if invalid?
+    return '無効な契約アンペアまたは電気使用量です。' if invalid?
 
     PLANS.map do |plan|
       {
         provider_name: plan.first,
-        plan_name: plan.last["plan_name"],
+        plan_name: plan.last['plan_name'],
         price: price(plan),
       }
     end
@@ -33,8 +33,8 @@ class Simulator
 
   private
     def price(plan)
-      return "対象外の契約電流のため計算できません。" unless plan.last["basic_charges"].keys.include?(@contract_ampere)
-      (basic_charge(plan.last["basic_charges"]) + commodity_charge(plan.last["commodity_charges"])).floor
+      return '対象外の契約電流のため計算できません。' unless plan.last['basic_charges'].keys.include?(@contract_ampere)
+      (basic_charge(plan.last['basic_charges']) + commodity_charge(plan.last['commodity_charges'])).floor
     end
 
     def basic_charge(basic_charges)
@@ -44,22 +44,22 @@ class Simulator
     end
 
     def commodity_charge(commodity_charges)
-      thresholds = commodity_charges["thresholds"]
-      charges = commodity_charges["charges"]
+      thresholds = commodity_charges['thresholds']
+      charges = commodity_charges['charges']
 
       if thresholds.nil?
         return @electric_energy_per_month * charges
       end
 
-      first_threshold = thresholds["first"]
-      second_threshold = thresholds["second"]
+      first_threshold = thresholds['first']
+      second_threshold = thresholds['second']
 
       if @electric_energy_per_month <= first_threshold
-        @electric_energy_per_month * charges["first"]
+        @electric_energy_per_month * charges['first']
       elsif @electric_energy_per_month > first_threshold && @electric_energy_per_month <= second_threshold
-        first_threshold * charges["first"] + (@electric_energy_per_month - first_threshold) * charges["second"]
+        first_threshold * charges['first'] + (@electric_energy_per_month - first_threshold) * charges['second']
       else
-        first_threshold * charges["first"] + second_threshold * charges["second"] + (@electric_energy_per_month - second_threshold) * charges["third"]
+        first_threshold * charges['first'] + second_threshold * charges['second'] + (@electric_energy_per_month - second_threshold) * charges['third']
       end
     end
 end
