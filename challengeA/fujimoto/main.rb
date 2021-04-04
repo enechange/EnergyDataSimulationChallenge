83cb4fb48f4aa12ba44fdc3ae0ac64231e60272c
fujimoon-k <fujimoon.k@gmail.com>
2021-04-04 21:52:19 +0900

    bundle exec rubocop -A

--- challengeA/fujimoto/main.rb
+++ challengeA/fujimoto/main.rb
@@ -2,37 +2,37 @@
 
 require './lib/simulator'
 
-puts "契約アンペア数を入力してください。"
-puts "＊10A, 15A, 20A, 30A, 40A, 50A, 60Aの中から選択してください。"
-puts "＊数字のみ入力してください"
+puts '契約アンペア数を入力してください。'
+puts '＊10A, 15A, 20A, 30A, 40A, 50A, 60Aの中から選択してください。'
+puts '＊数字のみ入力してください'
 amps = gets.to_i
 
 unless [10, 15, 20, 30, 40, 50, 60].include?(amps)
-  puts "契約アンペア数の入力に誤りがあります。"
-  exit
-end
- 
-puts "電力使用量(kWh)を入力してください"
-puts "数字のみ入力してください。"
-kWh = gets.to_i
-
-if kWh <= 0
-  puts "電力使用量(kWh)の入力に誤りがあります。"
+  puts '契約アンペア数の入力に誤りがあります。'
   exit
 end
 
-simulator = Simulator.new(amps, kWh)
-plan_list  = simulator.simulate
+puts '電力使用量(kWh)を入力してください'
+puts '数字のみ入力してください。'
+kwh = gets.to_i
 
-unless plan_list.empty?
-  puts "以下のプランからお選びいただけます。"
-  puts "========"
+if kwh <= 0
+  puts '電力使用量(kWh)の入力に誤りがあります。'
+  exit
+end
+
+simulator = Simulator.new(30, 150)
+plan_list = simulator.simulate
+
+if plan_list.empty?
+  puts 'ご案内できるプランはありません。'
+else
+  puts '以下のプランからお選びいただけます。'
+  puts '========'
   plan_list.each do |plan|
     puts "会社名：#{plan[:provider_name]}"
     puts "プラン名：#{plan[:plan_name]}"
     puts "月額目安：#{plan[:price]}円"
-    puts "========"
+    puts '========'
   end
-else
-  puts "ご案内できるプランはありません。"
 end
