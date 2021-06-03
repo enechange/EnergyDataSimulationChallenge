79a1285f2d9568fe6f9a6c8b5c6e5e76d28eaaba
k.yusuke <aseanchild1400@gmail.com>
2021-06-03 12:21:34 +0900

    ・電気を全く使用しない場合の条件を修正 ・電気料金計算ロジックをPlanクラスに移行

--- challengeA/katayama/plan.rb
+++ challengeA/katayama/plan.rb
@@ -25,20 +25,51 @@ class Plan
     @min_charge = args["min_charge"]
   end
 
+  # 電気料金を計算する
+  def calc_total_charge(amp, kwh)
+
+      # 基本料金の計算
+      base_charge = calc_base_charge(amp, kwh)
+
+      # 従量料金の計算
+      as_pay_charge = calc_as_pay_charge(kwh)
+      
+      # 電気料金 = 基本料金 + 従量料金(少数切り捨て)
+      total_charge = (base_charge + as_pay_charge).floor
+      
+      # 電気料金が月額最低額以下の場合、月額最低額で電気料金を計上する
+      # (現時点で東京電力エナジーパートナー 従量電灯Bのみ確認)
+      if !self.min_charge.nil? && self.min_charge > total_charge
+        total_charge = self.min_charge.floor
+      end
+
+      return total_charge
+  end
+
+  private 
+
   # 基本料金の算出を実行する
-  def calc_base_charge(input_amp)
+  def calc_base_charge(input_amp, kwh)
+    base_charge = 0
 
     # 入力されたアンペアがプラン対象かどうか判定
     if self.supported_amp.include?(input_amp)
 
       # 入力されたアンペアが対応可の場合、入力された契約アンペア数に対応する基本料金を算出する
       target_amp = self.base.find {|b| b["amp"] == input_amp}
-      return target_amp["charge"]
+      base_charge = target_amp["charge"]
+
+      # 電気使用量が0kWhの場合、電気料金のうち基本料金が半額になる
+      if kwh == 0  
+        base_charge = base_charge/2
+      end
+
+      return base_charge
     else
-      
+
       # 入力されたアンペアが対応外の場合
       self.is_supported = false
-      return 0
+      return base_charge
     end
   end
 
@@ -62,8 +93,6 @@ class Plan
     end
     return as_pay_sum
   end
-
-  private 
 
   # 従量料金の料金帯に含まれるか否か
   def is_range?(value, oppo)
