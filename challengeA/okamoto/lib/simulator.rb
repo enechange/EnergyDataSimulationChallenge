e7e777ead573a14f77b73e104d2922ef4fdda239
masaki okamoto <mochikichi@live.jp>
2021-06-20 12:36:08 +0900

    plans読み込みをJSONファイルからできるように

--- challengeA/okamoto/lib/simulator.rb
+++ challengeA/okamoto/lib/simulator.rb
@@ -1,3 +1,6 @@
+require 'json'
+JSON_FILE_PATH = '../data/plans.json'
+
 class Simulator
   attr_reader :contract_amp, :power_usage, :plans
 
@@ -16,7 +19,7 @@ class Plan
   attr_reader :contract_amp, :power_usage, :plans
 
   def initialize(contract_amp, power_usage)
-    @plans = plans
+    @plans = json_load
     @contract_amp = contract_amp
     @power_usage = power_usage
   end
@@ -27,14 +30,19 @@ class Plan
 
   private
 
+  def json_load
+    json_file_path = File.expand_path(JSON_FILE_PATH, __dir__)
+    JSON.load(File.open(json_file_path))
+  end
+
   def available?(plan)
-    plan[:basic_price].find { |price| price[:ampere] == contract_amp }
+    plan['basic_price'].find { |price| price['ampere'] == contract_amp }
   end
 
   def plan_with_price(plan)
     {
-      provider_name: plan[:provider_name],
-      plan_name: plan[:name],
+      provider_name: plan['provider_name'],
+      plan_name: plan['name'],
       price: sum_price(plan)
     }
   end
@@ -44,196 +52,27 @@ class Plan
   end
 
   def basic_price(plan)
-    plan[:basic_price].find { |price| price[:ampere] == contract_amp }[:price]
+    plan['basic_price'].find { |price| price['ampere'] == contract_amp }['price']
   end
 
   def pay_per_use_price(plan)
     sum = 0
     power_usage_before_stage = 0
     sorted_pay_per_use_price_lists(plan).each do |price_list|
-      power_usage_current_stage = power_usage - price_list[:min_kwh] - power_usage_before_stage
+      power_usage_current_stage = power_usage - price_list['min_kwh'] - power_usage_before_stage
       #丸め誤差が生じるため小数点第四位を四捨五入とする
-      sum += (price_list[:price_per_kwh] * power_usage_current_stage).round(3)
+      sum += (price_list['price_per_kwh'] * power_usage_current_stage).round(3)
       power_usage_before_stage += power_usage_current_stage
     end
     sum
   end
 
   def sorted_pay_per_use_price_lists(plan)
-    pay_per_use_price_lists(plan).sort_by {|list| list[:min_kwh]}.reverse
+    pay_per_use_price_lists(plan).sort_by {|list| list['min_kwh']}.reverse
   end
 
   def pay_per_use_price_lists(plan)
-    plan[:pay_per_use_price].select { |price| price[:min_kwh] <= power_usage }
-  end
-
-  def plans
-    [
-      {
-        name: '従量電灯B',
-        provider_name: '東京電力エナジーパートナー',
-        basic_price: [
-          {
-            "ampere": 10,
-            "price": 286.0
-          },
-          {
-            "ampere": 15,
-            "price": 429.0
-          },
-          {
-            "ampere": 20,
-            "price": 572.0
-          },
-          {
-            "ampere": 30,
-            "price": 858.0
-          },
-          {
-            "ampere": 40,
-            "price": 1144.0
-          },
-          {
-            "ampere": 50,
-            "price": 1430.0
-          },
-          {
-            "ampere": 60,
-            "price": 1716.0
-          }
-        ],
-        pay_per_use_price: [
-          {
-            "price_per_kwh": 19.88,
-            "min_kwh": 0
-          },
-          {
-            "price_per_kwh": 26.48,
-            "min_kwh": 120
-          },
-          {
-            "price_per_kwh": 30.57,
-            "min_kwh": 300
-          }
-        ]
-      },
-      {
-        name: 'おうちプラン',
-        provider_name: 'Looopでんき',
-        basic_price: [
-          {
-            "ampere": 10,
-            "price": 0
-          },
-          {
-            "ampere": 15,
-            "price": 0
-          },
-          {
-            "ampere": 20,
-            "price": 0
-          },
-          {
-            "ampere": 30,
-            "price": 0
-          },
-          {
-            "ampere": 40,
-            "price": 0
-          },
-          {
-            "ampere": 50,
-            "price": 0
-          },
-          {
-            "ampere": 60,
-            "price": 0
-          }
-        ],
-        pay_per_use_price: [
-          {
-            "price_per_kwh": 26.40,
-            "min_kwh": 0
-          }
-        ]
-      },
-      {
-        name: 'ずっとも電気１',
-        provider_name: '東京ガス',
-        basic_price: [
-          {
-            "ampere": 30,
-            "price": 858.0
-          },
-          {
-            "ampere": 40,
-            "price": 1144.0
-          },
-          {
-            "ampere": 50,
-            "price": 1430.0
-          },
-          {
-            "ampere": 60,
-            "price": 1716.0
-          }
-        ],
-        pay_per_use_price: [
-          {
-            "price_per_kwh": 23.67,
-            "min_kwh": 0
-          },
-          {
-            "price_per_kwh": 23.88,
-            "min_kwh": 140
-          },
-          {
-            "price_per_kwh": 26.41,
-            "min_kwh": 350
-          }
-        ]
-      },
-      {
-        name: '従量電灯Bたっぷりプラン',
-        provider_name: 'JXTGでんき',
-        basic_price: [
-          {
-            "ampere": 30,
-            "price": 858.0
-          },
-          {
-            "ampere": 40,
-            "price": 1144.0
-          },
-          {
-            "ampere": 50,
-            "price": 1430.0
-          },
-          {
-            "ampere": 60,
-            "price": 1716.8
-          }
-        ],
-        pay_per_use_price: [
-          {
-            "price_per_kwh": 19.88,
-            "min_kwh": 0
-          },
-          {
-            "price_per_kwh": 26.48,
-            "min_kwh": 120
-          },
-          {
-            "price_per_kwh": 25.08,
-            "min_kwh": 300
-          },
-          {
-            "price_per_kwh": 26.15,
-            "min_kwh": 600
-          }
-        ]
-      }
-    ]
+    plan['pay_per_use_price'].select { |price| price['min_kwh'] <= power_usage }
   end
 end
 
