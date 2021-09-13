1514a3aaac3023ec369fdd66c66f319278d7ddc8
tksk <takasaki@inagora.com>
2021-09-14 01:43:53 +0900

    initialize時にJSONデータ読み込み

--- challengeA/shogo_takasaki/src/simulator.rb
+++ challengeA/shogo_takasaki/src/simulator.rb
@@ -1,19 +1,26 @@
 # frozen_string_literal: true
 
+require 'json'
+
 # Simulator for electric usage
 class Simulator
-  attr_accessor :ampere, :usage
+  attr_accessor :ampere, :usage, :plans
 
   def initialize(ampere, usage)
     @ampere = ampere
     @usage = usage
+    File.open("plans.json") do |f|
+      @plans = JSON.load(f)
+    end
   end
 
   def simulate
-    [
-      { provider_name: 'Looopでんき', plan_name: 'おうちプラン', price: '1234' },
-      { provider_name: 'Looopでんき2', plan_name: 'おうちプラン4', price: '12345' },
-      { provider_name: 'Looopでんき3', plan_name: 'おうちプラン5', price: '12346' }
-    ]
+    @plans['suppliers'].map do |plan|
+      {
+        provider_name: plan['provider_name'],
+        plan_name: plan['plan_name'],
+        price: plan.price(@ampere, @usage)
+      }
+    end
   end
 end
