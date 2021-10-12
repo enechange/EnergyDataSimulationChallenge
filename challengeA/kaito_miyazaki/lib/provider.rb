7fff106a9062b6863767e6e1d2508ee347e6dde5
texpandafter <texpandafter@gmail.com>
2021-10-12 23:11:20 +0900

    add: Providerクラスの作成

---
+++ challengeA/kaito_miyazaki/lib/provider.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative 'plan'
+
+# プロバイダークラス
+class Provider
+  attr_reader :name, :plans
+
+  # plansの例：[ { 'plan_name' => 'PLAN_NAME_EXAMPLE',
+  #               'charge_rules' => { 'basic_charge_rule' => { ... }, 'usage_charge_rule' => { ... } } }, ... ]
+  def initialize(name, plans)
+    @name = name
+    @plans = []
+    plans.each do |plan|
+      @plans << Plan.new(plan['plan_name'], plan['charge_rules'])
+    end
+  end
+end
