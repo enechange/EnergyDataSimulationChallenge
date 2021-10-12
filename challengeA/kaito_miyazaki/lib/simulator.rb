71a58c6b8e27e57c5d0bfae0b66b1b283ecfa7fa
texpandafter <texpandafter@gmail.com>
2021-10-12 23:42:43 +0900

    add: Simulatorクラスの作成

---
+++ challengeA/kaito_miyazaki/lib/simulator.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'yaml'
+require_relative 'provider'
+
+# プロバイダーのデータの読み込み
+PROVIDERS = open('./providers.yaml', 'r') { |f| YAML.safe_load(f) }
+
+# シミュレータークラス
+class Simulator
+  # クラスインスタンス変数@providersの定義
+  @providers = []
+  class << self
+    attr_reader :providers
+  end
+
+  PROVIDERS.each do |provider_name, plans|
+    @providers << Provider.new(provider_name, plans)
+  end
+
+  # 契約アンペア数（@amp），使用量（@kwh）をインスタンス変数として持つ
+  def initialize(amp, kwh)
+    @amp = amp
+    @kwh = kwh
+  end
+
+  # 契約アンペア数，使用量から，各プランについて料金を計算し，
+  # { provider_name: プロバイダー名, plan_name: プラン名, price: 料金 }の形式のhashを要素とする配列として返す
+  def simulate
+    results = []
+    self.class.providers.each do |provider|
+      provider.plans.each do |plan|
+        price = plan.price(@amp, @kwh)
+        # 料金が計算できないプランについては，配列に含めない
+        results << { provider_name: provider.name, plan_name: plan.name, price: price } unless price.nan?
+      end
+    end
+    results
+  end
+end
